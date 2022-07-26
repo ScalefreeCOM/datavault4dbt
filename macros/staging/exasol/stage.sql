9e9ebf205e384bcf5e8c89cdb206f09202f33385
tkirschke <tkirschke@scalefree.com>
2022-07-26 11:11:27 +0200

    Usage of dbdtvault_scalefree prefix in global variables

--- macros/staging/exasol/stage.sql
+++ macros/staging/exasol/stage.sql
@@ -72,12 +72,12 @@
 {%- set final_columns_to_select = final_columns_to_select + source_columns_to_select -%}
 
 {#- Select hashing algorithm -#}
-{%- set hash = var('hash', 'MD5') -%}
+{%- set hash = var('dbtvault_scalefree.hash', 'MD5') -%}
 {%- set hash_alg, unknown_key, error_key = dbtvault_scalefree.hash_default_values(hash_function=hash) -%}
 
-{%- set beginning_of_all_times = var('beginning_of_all_times', '0001-01-01T00-00-01') -%}
-{%- set end_of_all_times = var('end_of_all_times', '8888-12-31T23-59-59') -%}
-{%- set timestamp_format = var('timestamp_format', 'YYYY-mm-ddTHH-MI-SS') -%}
+{%- set beginning_of_all_times = var('dbtvault_scalefree.beginning_of_all_times', '0001-01-01T00-00-01') -%}
+{%- set end_of_all_times = var('dbtvault_scalefree.end_of_all_times', '8888-12-31T23-59-59') -%}
+{%- set timestamp_format = var('dbtvault_scalefree.timestamp_format', 'YYYY-mm-ddTHH-MI-SS') -%}
 
 WITH
 
