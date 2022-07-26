c9addc0918954755f0f1aca8a14247c95f62112c
tkirschke <tkirschke@scalefree.com>
2022-07-26 14:39:47 +0200

    First version of multi source Hub, adds source_model column

--- macros/tables/bigquery/hub.sql
+++ macros/tables/bigquery/hub.sql
@@ -1,7 +1,6 @@
-{%- macro hub(hashkey, business_key, src_ldts, src_rsrc, source_models) -%}
+{%- macro hub(hashkey, src_ldts, src_rsrc, source_models) -%}
 
     {{ return(adapter.dispatch('hub', 'dbtvault_scalefree')(hashkey=hashkey,
-                                                  business_key=business_key,
                                                   src_ldts=src_ldts,
                                                   src_rsrc=src_rsrc,
                                                   source_models=source_models)) }}
@@ -13,24 +12,24 @@
 {%- set end_of_all_times = var('dbtvault_scalefree.end_of_all_times', '8888-12-31T23-59-59') -%}
 {%- set timestamp_format = var('dbtvault_scalefree.timestamp_format', '%Y-%m-%dT%H-%M-%S') -%}
 
-{%- set ns = namespace(last_cte= "") -%}
+{%- set ns = namespace(last_cte= "", bk_columns = []) -%}
 
 {# Select the Business Key column from the first source model definition provided in the hub model and put them in an array. #}
-{%- set bk_columns = [] %}
-{%- for source_model in source_models.keys() %}
+
+{%- for source_model in source_models.keys() %}    
+
+    {%- set bk_column_input = source_models[source_model]['bk_column'] -%}
+
+    {%- if not (bk_column_input is iterable and bk_column_input is not string) -%}
+
+        {%- set bk_column_input = [bk_column_input] -%}
+        {%- do source_models[source_model].update({'bk_columns': bk_column_input}) -%}
+
+    {%- endif -%}
 
     {%- if loop.index == 1 -%}
 
-        {%- set bk_column_names = source_models[source_model]['bk_column'] -%}
-
-        {%- if not (bk_column_names is iterable and bk_column_names is not string) -%}
-
-            {%- set bk_column_names = [bk_column_names] -%}
-            {%- do source_models[source_model].update('bk_columns': [bk_column_names]) -%}
-
-        {%- endif -%}
-
-        {% set bk_columns = bk_columns + bk_column_names %}
+        {% set ns.bk_columns = ns.bk_columns + bk_column_input %}
 
     {%- endif -%}
 
@@ -45,11 +44,11 @@
 WITH
 
 
-{%- if is_incremental() -%}
+{% if is_incremental() -%}
 distinct_target_hashkeys AS (
     
     SELECT DISTINCT
-        {{ hashkey_column }}
+        {{ hashkey }}
     FROM {{ this }}
 
 ),
@@ -63,39 +62,38 @@ max_ldts_per_source_in_target AS (
     WHERE {{ src_ldts }} != {{ dbtvault_scalefree.string_to_timestamp(timestamp_format, end_of_all_times) }}
     GROUP BY source_model
 
-),
-{%- endif -%}
+), 
+{% endif -%}
 
-{%- for source_model in source_models.keys() -%}
+{%- for source_model in source_models.keys() %}
 
     {%- set source_number = loop.index | string -%}
 
-    {%- if 'hk_column' not in source_models[source_model].keys() -%}
+    {%- if 'hk_column' not in source_models[source_model].keys() %}
         {%- set hk_column = hashkey -%}
     {%- else -%}
         {%- set hk_column = source_models[source_model]['hk_column'] -%}
-    {%- endif -%}
-
+    {% endif %}
 
     src_new_{{ source_number }} AS (
 
         SELECT 
-            {{ hk_column }} AS {{ hashkey_column}},
+            {{ hk_column }} AS {{ hashkey }},
 
-            {%- for bk in source_models[source_model]['bk_columns'] -%}
+            {% for bk in source_models[source_model]['bk_columns']|list -%}
             {{ bk }},
-            {%- endfor -%}
+            {%- endfor %}
 
             {{ src_ldts }},
             {{ src_rsrc }},
-            {{ source_model }} AS source_model
+            '{{ source_model }}' AS source_model
         FROM {{ ref(source_model) }} src
 
-        {%- if is_incremental() -%}
+        {%- if is_incremental() %}
         INNER JOIN max_ldts_per_source_in_target max 
-            ON max.source_model == '{{ source_model }}'
+            ON max.source_model = '{{ source_model }}'
         WHERE src.{{ src_ldts }} > max.max_ldts
-        {%- endif -%}
+        {%- endif %}
 
         QUALIFY ROW_NUMBER() OVER (PARTITION BY {{ hk_column }} ORDER BY {{ src_ldts }}) = 1
 
@@ -113,11 +111,11 @@ source_new_union AS (
     {%- set source_number = loop.index | string -%}
 
     SELECT
-        {{ hashkey_column }},
+        {{ hashkey }},
 
-        {%- for bk in source_models[source_model]['bk_columns'] -%}
-            {{ bk }} AS {{ bk_columns[loop.index - 1] }},
-        {%- endfor -%}
+        {% for bk in source_models[source_model]['bk_columns']|list %}
+            {{ bk }} AS {{ ns.bk_columns[loop.index - 1] }},
+        {% endfor -%}
 
         {{ src_ldts }},
         {{ src_rsrc }},
@@ -126,7 +124,7 @@ source_new_union AS (
 
     {%- if not loop.last %}
     UNION ALL
-    {%- endif %}
+    {% endif -%}
 
     {%- endfor -%}
 
@@ -140,13 +138,13 @@ earliest_hk_over_all_sources AS (
         lcte.*
     FROM {{ ns.last_cte }} AS lcte
 
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY {{ hashkey_column }} ORDER BY {{ src_ldts }}) = 1
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY {{ hashkey }} ORDER BY {{ src_ldts }}) = 1
 
     {%- set ns.last_cte = 'earliest_hk_over_all_sources' -%}
 
 ),
 
-{%- endif -%}
+{%- endif %}
 
 records_to_insert AS (
 
@@ -155,7 +153,7 @@ records_to_insert AS (
     FROM {{ ns.last_cte }} AS lcte
 
     {%- if is_incremental() %}
-    WHERE lcte.{{ hashkey_column }} NOT IN distinct_target_hashkeys
+    WHERE lcte.{{ hashkey }} NOT IN (SELECT * FROM distinct_target_hashkeys)
     {% endif -%}
 )
 
