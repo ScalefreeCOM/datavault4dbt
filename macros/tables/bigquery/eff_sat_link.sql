3106a6ba19394569284cea5b1d5ca79becb99536
tkirschke <tkirschke@scalefree.com>
2022-08-08 12:59:59 +0200

    Created the (hopefully) final version of the eff_sat_link macro

--- macros/tables/bigquery/eff_sat_link.sql
+++ macros/tables/bigquery/eff_sat_link.sql
@@ -1,65 +1,154 @@
+{%- macro default__eff_sat_link(link_hashkey, driving_key, secondary_fks, src_ldts, src_rsrc, source_model) -%}
+
+{{- dbtvault.check_required_parameters(link_hashkey=link_hashkey, driving_key=driving_key, secondary_fks=secondary_fks,
+                                       src_ldts=src_ldts, src_rsrc=src_rsrc,
+                                       source_model=source_model) -}}
+
+{%- set source_cols = dbtvault.expand_column_list(columns=[link_hashkey, driving_key, secondary_fks, src_rsrc, src_ldts]) -%}
+{%- set union_cols = dbtvault.expand_column_list(columns=[link_hashkey, driving_key, secondary_fks, src_rsrc]) -%}
+{%- set final_cols = dbtvault.expand_column_list(columns=[link_hashkey, driving_key, secondary_fks, src_ldts, src_rsrc]) -%}
+
+
+WITH
+
+{# 
+    Get all records from staging layer where driving key and secondary foreign keys are not null.
+    Deduplicate over HK+Driving Key uneuqls the previous (regarding src_ldts) combination. 
+#}
+stage AS (
+    SELECT
+        {{ dbtvault.prefix(source_cols, 'source') }}
+    FROM {{ ref(source_model) }} AS source
+    WHERE {{ dbtvault.multikey(driving_key, prefix='source', condition='IS NOT NULL') }}
+    AND {{ dbtvault.multikey(secondary_fks, prefix='source', condition='IS NOT NULL') }}
+    QUALIFY CASE WHEN {{ dbtvault.prefix([link_hashkey], 'source') }} = LAG({{ dbtvault.prefix([link_hashkey], 'source') }}) OVER (PARTITION BY {{ dbtvault.prefix([driving_key], 'source') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'source') }}) THEN FALSE
+                 ELSE TRUE
+            END
+),
+
+{%- if is_incremental() %}
+
 {#
-    This Macro creates a virtual satellite for hubs, that creates a timeline in which each Driving Key has one active relationship
-    at a time. Therefor it calculates a start_timestamp and end_timestamp for each entry in the connected link.
-
-    Parameters:
-
-    link::string                    Name of the link model this sat should be attached to.
-    link_hk::string                 Name of the PK column of the link. The Hashkey column over all Business Keys.
-    driving_key::string             Name of the Driving Key column inside the link.
-    sec_fks::string|list(string)    Name(s) of all secondary foreign key columns inside the link. Would be all foreign keys except the Driving Key. 
-    ldts::string                    Name of the ldts column inside the link
-    rsrc::string                    Name of the rsrc column inside the link
-
+    Get the latest record for each driving key, already existing in eff_sat. Only applied if incremental.
 #}
 
-
-{%- macro link_eff_sat(link, link_hk, driving_key, sec_fks, ldts, rsrc) -%}
-
-    {{ return(adapter.dispatch('link_eff_sat', 'dbtvault_scalefree')(link=link, link_hk=link_hk, driving_key=driving_key, 
-                                                                     sec_fks=sec_fks, ldts=ldts, rsrc=rsrc)) }}
-
-{%- endmacro -%}                                                                     
-
-
-{%- macro default__link_eff_sat(link, link_hk, driving_key, sec_fks, ldts, rsrc) -%}
-
-{{- dbtvault_scalefree.check_required_parameters(link=link, link_hk=link_hk, driving_key=driving_key,
-                                                sec_fks=sec_fks, ldts=ldts, rsrc=rsrc) -}}
-
-{%- set source_cols = dbtvault_scalefree.expand_column_list(columns=[hashkey_column, driving_key, sec_fks, ldts, rsrc]) -%}
-
-{%- set link_relation = ref(link) -%}
-
-{%- set start_date_alias = var('dbtvault_scalefree.start_date_alias', 'valid_from') -%}
-{%- set end_date_alias = var('dbtvault_scalefree.end_date_alias', 'valid_to') -%}
-
-{%- set end_of_all_times = var('dbtvault_scalefree.end_of_all_times', '8888-12-31T23-59-59') -%}
-{%- set timestamp_format = var('dbtvault_scalefree.timestamp_format', '%Y-%m-%dT%H-%M-%S') -%}
-
-{%- if not dbtvault_scalefree.is_list(sec_fks) -%}
-    {%- set sec_fks = [sec_fks] -%}
-{%- endif -%}
-
-{{ dbtvault_scalefree.prepend_generated_by() }}
-
-WITH 
-
-eff_sat AS (
-
+latest_record AS (
     SELECT 
-        {{ link_hk }},
-        {{ driving_key }},
-        {%- for fk in sec_fks %}
-            {{ fk }},
-        {% endfor -%}
-        {{ rsrc }},
-        {{ ldts }} AS {{ start_date_alias }},
-        COALESCE(LEAD(TIMESTAMP_SUB({{ ldts }}, INTERVAL 1 MICROSECOND)) OVER (PARTITION BY {{ driving_key }} ORDER BY {{ ldts }}),{{ dbtvault_scalefree.string_to_timestamp( timestamp_format , end_of_all_times) }}) as {{ end_date_alias }}
-    FROM {{ link_relation }}
+        {{ dbtvault.prefix(source_cols, 'current_records') }}
+    FROM {{ this }} AS current_records
+    INNER JOIN (
+        SELECT DISTINCT 
+            {{ dbtvault.prefix([driving_key], 'stage') }}
+        FROM stage
+    ) AS source_records
+        ON {{ dbtvault.multikey(driving_key, prefix=['current_records', 'source_records'], condition='=') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY {{ dbtvault.prefix([driving_key], 'current_records') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'current_records') }} DESC) = 1
+),
+{%- endif %}
 
+{#
+    Select only incoming records from the stage, that are newer than the latest record in the eff_sat, or when it does not exist yet.
+    Creates the src_ldts_lead for intermediate changes, and a rank column over the driving key, order by the ldts. 
+#}
+stage_new AS (
+    SELECT
+        {{ dbtvault.prefix(source_cols, 'stage') }}
+    ,   LEAD({{ dbtvault.prefix([src_ldts], 'stage') }}) OVER (PARTITION BY {{ dbtvault.prefix([driving_key], 'stage') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'stage') }}) AS src_ldts_lead
+    ,   ROW_NUMBER() OVER (PARTITION BY {{ dbtvault.prefix([driving_key], 'stage') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'stage') }}) as stage_rank
+    FROM stage
+    {%- if is_incremental() %}
+    LEFT JOIN latest_record
+        ON {{ dbtvault.multikey(driving_key, prefix=['stage', 'latest_record'], condition='=') }}
+    WHERE {{ dbtvault.prefix([src_ldts], 'stage') }} > {{ dbtvault.prefix([src_ldts], 'latest_record') }}
+        OR {{ dbtvault.prefix([src_ldts], 'latest_record') }} IS NULL
+    {%- endif %}
+),
+
+{%- if is_incremental() -%}
+
+{#
+    Disable all latest records in the eff_sat, when there is a new relationship for that driving key in the stage.
+#}
+
+deactivated_existing AS (
+
+    SELECT
+        {{ dbtvault.prefix(union_cols, 'latest_record') }},
+        {{ dbtvault.prefix([src_ldts], 'stage_new') }} AS {{ src_ldts }},
+        FALSE AS is_active
+    FROM latest_record
+    LEFT JOIN stage_new
+        ON {{ dbtvault.multikey(driving_key, prefix=['latest_record', 'stage_new'], condition='=') }}
+    WHERE {{ dbtvault.prefix([link_hashkey], 'latest_record') }} != {{ dbtvault.prefix([link_hashkey], 'stage_new') }}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY {{ dbtvault.prefix([driving_key], 'stage_new') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'stage_new') }}) = 1
+
+),
+
+{%- endif %}
+
+{#
+    Activate all rows that have a different relationship for an existing driving key OR where the driving key is not yet existing in the eff_sat 
+    OR is not the first new one in the incoming data (due to intermediate changes).
+#}
+
+activated_new_records AS (
+
+    SELECT
+        {{ dbtvault.prefix(union_cols, 'stage_new') }},
+        {{ dbtvault.prefix([src_ldts], 'stage_new') }} AS {{ src_ldts }},
+        TRUE AS is_active
+    FROM stage_new
+    {%- if is_incremental() %}
+    LEFT JOIN latest_record
+        ON {{ dbtvault.multikey(driving_key, prefix=['stage_new', 'latest_record'], condition='=') }}
+    WHERE {{ dbtvault.prefix([link_hashkey], 'stage_new') }} != {{ dbtvault.prefix([link_hashkey], 'latest_record') }}
+        OR {{ dbtvault.prefix([src_ldts], 'latest_record') }} IS NULL
+        OR stage_new.stage_rank != 1
+    {%- endif %}
+
+),
+
+{#
+    Deactivate all intermediate changes that are not the latest one. 
+#}
+
+deactivated_intermediates AS (
+
+    SELECT
+        {{ dbtvault.prefix(union_cols, 'stage_new') }},
+        stage_new.src_ldts_lead AS src_ldts,
+        FALSE AS is_active
+    FROM stage_new
+    {%- if is_incremental() %}
+    LEFT JOIN latest_record
+        ON {{ dbtvault.multikey(driving_key, prefix=['stage_new', 'latest_record'], condition='=') }}
+    WHERE {{ dbtvault.prefix([link_hashkey], 'stage_new') }} != {{ dbtvault.prefix([link_hashkey], 'latest_record') }}
+        OR {{ dbtvault.prefix([src_ldts], 'latest_record') }} IS NULL
+        OR stage_new.stage_rank != 1
+    {%- endif %}
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY {{ dbtvault.prefix([driving_key], 'stage_new') }} ORDER BY {{ dbtvault.prefix([src_ldts], 'stage_new') }} DESC) != 1
+
+),
+
+{#
+    Unionize all three cases for final insertion.
+#}
+
+final_columns_to_select AS (
+
+    SELECT * FROM activated_new_records
+
+    UNION ALL 
+
+    SELECT * FROM deactivated_intermediates
+
+    {% if is_incremental() -%}
+    UNION ALL
+
+    SELECT * FROM deactivated_existing
+    {%- endif %}
 )
 
-SELECT * FROM eff_sat
+SELECT * FROM final_columns_to_select
 
-{%- endmacro -%}
+{% endmacro %}
