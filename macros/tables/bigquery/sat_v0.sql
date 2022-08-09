450ff185d0ffa7947c0f0a475f28f5d0304cbf7b
tkirschke <tkirschke@scalefree.com>
2022-08-09 16:58:49 +0200

    Reformatted and commented sat_v0

--- macros/tables/bigquery/sat_v0.sql
+++ macros/tables/bigquery/sat_v0.sql
@@ -1,15 +1,4 @@
-{%- macro sat_v0(src_pk, src_hashdiff, src_payload, src_ldts, src_rsrc, source_model) -%}
-
-    {{ adapter.dispatch('sat_v0', 'dbtvault_scalefree')(src_pk=src_pk, 
-                                         src_hashdiff=src_hashdiff,
-                                         src_payload=src_payload,
-                                         src_ldts=src_ldts,
-                                         src_rsrc=src_rsrc,
-                                         source_model=source_model) }}
-
-{%- endmacro -%}                                         
-
-{%- macro default__sat_v0(src_pk, src_hashdiff, src_payload, src_ldts, src_rsrc, source_model) -%}
+{%- macro default__sat_v0(parent_hashkey, src_hashdiff, src_payload, src_ldts, src_rsrc, source_model) -%}
 
 {%- set hash = var('dbtvault_scalefree.hash', 'MD5') -%}
 {%- set hash_alg, unknown_key, error_key = dbtvault_scalefree.hash_default_values(hash_function=hash) -%}
@@ -18,73 +7,83 @@
 {%- set end_of_all_times = var('dbtvault_scalefree.end_of_all_times', '8888-12-31T23-59-59') -%}
 {%- set timestamp_format = var('dbtvault_scalefree.timestamp_format', '%Y-%m-%dT%H-%M-%S') -%}
 
-{{ prepend_generated_by() }}
+{%- set source_cols = dbtvault_scalefree.expand_column_list(columns=[parent_hashkey, src_hashdiff, src_ldts, src_rsrc, src_payload]) -%}
 
+{%- set source_relation = ref(source_model) -%}
 
+{{ dbtvault_scalefree.prepend_generated_by() }}
 
 WITH
-{%- if is_incremental() %}
-target AS (
+
+{# Selecting all source data, that is newer than latest data in sat if incremental #}
+source_data AS (
+
     SELECT
-        {{ src_pk|lower }},
+        {{ dbtvault_scalefree.print_list(source_cols) }}
+    FROM {{ source_relation }}
+
+    {%- if is_incremental() %}
+    WHERE {{ src_ldts }} > (
+        SELECT 
+            MAX({{ src_ldts }}) FROM {{ this }}
+        WHERE {{ src_ldts }} != {{ dbtvault_scalefree.string_to_timestamp(timestamp_format, end_of_all_times) }}
+    )
+    {%- endif %}
+),
+
+{% if is_incremental() -%}
+{# Get the latest record for each parent hashkey in existing sat, if incremental. #}
+latest_entries_in_sat AS (
+
+    SELECT
+        {{ parent_hashkey }},
         {{ src_hashdiff }}
     FROM {{ this }}
-    WHERE {{ src_pk|lower }} IN (
-        SELECT {{ src_pk|lower }} 
-        FROM {{ ref(source_model) }}
-        WHERE {{ src_ldts }} > (
-            SELECT 
-                MAX({{ src_ldts }}) FROM {{ this }}
-            WHERE {{ src_ldts }} != PARSE_TIMESTAMP('{{ timestamp_format }}', '{{ end_of_all_times }}')
-            )
-        )
-    QUALIFY ROW_NUMBER() OVER(PARTITION BY {{ src_pk|lower }} ORDER BY {{ src_ldts }} DESC) = 1
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY {{ parent_hashkey|lower }} ORDER BY {{ src_ldts }} DESC) = 1
+    
     ),
-{% endif %}
 
-base AS (
+{%- endif %}
 
-    SELECT
-        {{ src_pk|lower }},
-        {{ src_ldts }},
-        {{ src_hashdiff }},
-        {{ src_rsrc }},
+{# 
+    Deduplicate source by comparing each hashdiff to the hashdiff of the previous record, for each hashkey. 
+    Additionally adding a row number based on that order, if incremental. 
+#}
+deduplicated_numbered_source AS (
+
+    SELECT 
+        {{ dbtvault_scalefree.print_list(source_cols) }}
     {% if is_incremental() %}
-        ROW_NUMBER() OVER(PARTITION BY {{ src_pk|lower }} ORDER BY {{ src_ldts }}) as rn,
+        ,ROW_NUMBER() OVER(PARTITION BY {{ parent_hashkey|lower }} ORDER BY {{ src_ldts }}) as rn,
     {%- endif -%}
-    {%- for col in src_payload %}
-        {{ col }}
-        {{- ',' if not loop.last -}}
-    {%- endfor %}
-    FROM {{ ref(source_model) }}
-    {%- if is_incremental() %}
-    WHERE {{ src_ldts }} > (SELECT MAX({{ src_ldts }}) FROM {{ this }})
-    {%- endif -%}
-QUALIFY CASE
-            WHEN {{ src_hashdiff }} = LAG({{ src_hashdiff }}) OVER(PARTITION BY {{ src_pk|lower }} ORDER BY {{ src_ldts }}) THEN FALSE
+    FROM source_data   
+    QUALIFY 
+        CASE
+            WHEN {{ src_hashdiff }} = LAG({{ src_hashdiff }}) OVER(PARTITION BY {{ parent_hashkey|lower }} ORDER BY {{ src_ldts }}) THEN FALSE
             ELSE TRUE
-        END
+        END     
+),
 
-)
+{# 
+    Select all records from the previous CTE. If incremental, compare the oldest incoming entry to
+    the existing records in the satellite. 
+#}
+records_to_insert AS (
 
-SELECT 
-    {{ src_pk|lower }},
-    {{ src_ldts }},
-    {{ src_hashdiff }},
-    {{ src_rsrc }},
-    {%- for col in src_payload %}
-    {{ col }}
-    {{- ',' if not loop.last -}}
-    {%- endfor %}
-FROM base
-WHERE {{ src_pk|lower }} != '{{ error_key }}'
-{%- if is_incremental() %}
-{# Check if each record is already in the target CTE, if yes, then no delta, if no, then delta #}
-    AND NOT EXISTS (SELECT 1 
-                    FROM target
-                    WHERE target.{{ src_pk|lower }} = base.{{ src_pk|lower }}
-                        AND target.{{ src_hashdiff }} = base.{{ src_hashdiff }}
-                        AND base.rn = 1)
-{%- endif -%}                        
+    SELECT 
+        {{ dbtvault_scalefree.print_list(source_cols) }}
+    FROM deduplicated_numbered_source
+    {%- if is_incremental() %}
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM latest_entries_in_sat
+        WHERE {{ dbtvault_scalefree.multikey(parent_hashkey, prefix=['latest_entries_in_sat', 'deduplicated_numbered_source'], condition='=') }}
+            AND {{ dbtvault_scalefree.multikey(src_hashdiff, prefix=['latest_entries_in_sat', 'deduplicated_numbered_source'], condition='=') }}
+            AND deduplicated_numbered_source.rn = 1)
+    {%- endif %}
+    
+    )
+
+SELECT * FROM records_to_insert                      
  
 {%- endmacro -%}
