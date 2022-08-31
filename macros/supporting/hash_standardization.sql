8ac93058a28124bbe3459b97bce19304db56d11e
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2022-08-31 07:40:15 +0200

    Update hash_standardization.sql
    Removed duplicate default__attribute_standardise(), Removed unused variables in exasol__atribute_standardise @bschlottfeldt

--- macros/supporting/hash_standardization.sql
+++ macros/supporting/hash_standardization.sql
@@ -1,11 +1,6 @@
 {% macro attribute_standardise() %}
         {{- adapter.dispatch('attribute_standardise', 'dbtvault_scalefree')() -}}
 {% endmacro %}
-{%- macro default__attribute_standardise() -%}
-
-{{ return(adapter.dispatch('attribute_standardise', 'dbtvault_scalefree')()) }}
-
-{%- endmacro -%}
 
 {%- macro default__attribute_standardise() -%}
 
@@ -20,9 +15,7 @@ CONCAT('\"', REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(TRIM(CAST([EXPRESSION] AS STR
 {%- endmacro -%}
 
 {%- macro exasol__attribute_standardise() -%}
-{%- set concat_string = var('concat_string', '||') -%}
-{%- set quote = var('quote', '"') -%}
-{%- set null_placeholder_string = var('null_placeholder_string', '^^') -%}
+
 CONCAT('"', REPLACE(REPLACE(REPLACE(TRIM(CAST([EXPRESSION] AS VARCHAR(20000) UTF8 )), '\\\', '\\\\\'), '[QUOTE]', '"'), '[NULL_PLACEHOLDER_STRING]', '--'), '\"')
 
 {%- endmacro -%}
