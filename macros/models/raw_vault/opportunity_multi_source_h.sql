17a381097278b5eee2b68a4c416a2740a6c8b769
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2022-09-14 16:43:13 +0200

    Catching the case when rsrc_static is not defined, and when nothing in rsrc static is static

--- macros/models/raw_vault/opportunity_multi_source_h.sql
+++ macros/models/raw_vault/opportunity_multi_source_h.sql
@@ -8,6 +8,25 @@
                                                     'rsrc_static': '*/SALESFORCE/06sIPY/Opportunity/*'},
                                 'stage_account': {'bk_columns': 'account_key__c',
                                                     'hk_column': 'hk_account_h',
-                                                    'rsrc_static': '*/SALESFORCE/06sIPY/Account/*'}},
+                                                    rsrc_all_static: false} },
+                 src_ldts='ldts',
+                 src_rsrc='rsrc') }}
+
+{{ dbtvault_scalefree.hub(hashkey='hk_opportunity_h',
+                 source_models={'stage_opportunity': {'bk_columns': 'opportunity_key__c',
+                                                    'rsrc_static': '*/SALESFORCE/06sIPY/Opportunity/*'},
+                                'stage_account': {'bk_columns': 'account_key__c',
+                                                    'hk_column': 'hk_account_h',
+                                                    rsrc_is_static: true} },
+                 src_ldts='ldts',
+                 src_rsrc='rsrc') }}
+
+
+{{ dbtvault_scalefree.hub(hashkey='hk_opportunity_h',
+                 source_models={'stage_opportunity': {'bk_columns': 'opportunity_key__c',
+                                                    'rsrc_static': '*/SALESFORCE/06sIPY/Opportunity/*'},
+                                'stage_account': {'bk_columns': 'account_key__c',
+                                                    'hk_column': 'hk_account_h',
+                                                    'rsrc_static': '*'} }, --- either rsrc is completely static or nothing in it is static
                  src_ldts='ldts',
                  src_rsrc='rsrc') }}
