2db422f2168be8f7bd727ca5d72773512e622e8d
suhitadutta <73947468+suhitadutta@users.noreply.github.com>
2022-09-22 15:31:26 +0200

    Override generate_schema_name in project

---
+++ macros/supporting/generate_schema_name.sql
@@ -0,0 +1,17 @@
+{# Include this macro in your dbt project to override the default settings
+   Note: Retaining or editing this macro in the package has no impact on your dbt project  #}
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
