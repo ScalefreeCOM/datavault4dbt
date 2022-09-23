0ad2d2d6c64649a4958848ae052218165d01a63d
suhitadutta <73947468+suhitadutta@users.noreply.github.com>
2022-09-23 14:26:01 +0200

    Removed the references of hash glob var

--- macros/tables/snowflake/sat_v1.sql
+++ macros/tables/snowflake/sat_v1.sql
@@ -2,9 +2,6 @@
 
 {%- set end_of_all_times = var('dbtvault_scalefree.end_of_all_times','8888-12-31T23-59-59') -%}
 {%- set timestamp_format = var('dbtvault_scalefree.timestamp_format','%Y-%m-%dT%H-%M-%S') -%}
-{%- set hash = var('dbtvault_scalefree.hash', 'MD5') -%}
-{%- set hash_dtype = var('dbtvault_scalefree.hash_datatype', 'STRING') -%}
-{%- set hash_alg, unknown_key, error_key = dbtvault_scalefree.hash_default_values(hash_function=hash,hash_datatype=hash_dtype) -%}
 
 {%- set source_relation = ref(sat_v0) -%}
 {%- set all_columns = dbtvault_scalefree.source_columns(source_relation=source_relation) -%}
