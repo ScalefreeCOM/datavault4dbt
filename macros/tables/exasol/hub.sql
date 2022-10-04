52612de63537095851faafc9e8961c6e448a02c8
Barbara Schlottfeldt <101649281+bschlottfeldt@users.noreply.github.com>
2022-10-04 19:35:19 +0200

    Modified use of timestamp variables
    Modified the use of these following variables to use the 'exasol' key since now they are defined as dictionaries in dbt_project.yml
    - end_of_all_times
    - timestamp_format

--- macros/tables/exasol/hub.sql
+++ macros/tables/exasol/hub.sql
@@ -1,8 +1,8 @@
 
 {%- macro exasol__hub(hashkey, business_keys, src_ldts, src_rsrc, source_models) -%}
 
-{%- set end_of_all_times = var('datavault4dbt.end_of_all_times', '8888-12-31T23-59-59') -%}
-{%- set timestamp_format = var('datavault4dbt.timestamp_format', 'YYYY-mm-ddTHH-MI-SS') -%}
+{%- set end_of_all_times = var('datavault4dbt.end_of_all_times', '8888-12-31 23:59:59') -%}
+{%- set timestamp_format = var('datavault4dbt.timestamp_format', 'YYYY-mm-dd HH:MI:SS') -%}
 
 {%- set ns = namespace(last_cte= "", source_included_before = {}, has_rsrc_static_defined=true, source_models_rsrc_dict={}) -%}
 
@@ -143,7 +143,7 @@ WITH
                 rsrc_static,
                 MAX({{ src_ldts }}) as max_ldts
             FROM {{ ns.last_cte }}
-            WHERE {{ src_ldts }} != {{ datavault4dbt.string_to_timestamp(timestamp_format, end_of_all_times) }}
+            WHERE {{ src_ldts }} != {{ datavault4dbt.string_to_timestamp(timestamp_format['exasol'], end_of_all_times['exasol']) }}
             GROUP BY rsrc_static
 
         ),
