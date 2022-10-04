711ea023d98dc0eae2628406afd5f61c38e331e7
Barbara Schlottfeldt <101649281+bschlottfeldt@users.noreply.github.com>
2022-10-04 19:32:42 +0200

    Modified use of timestamp global variables
    These variables now use the key 'exasol' since they are defined in dbt_project.yml as a dictionary:
    - end_of_all_times
    - timestamp_format

--- macros/tables/exasol/eff_sat_link_v1.sql
+++ macros/tables/exasol/eff_sat_link_v1.sql
@@ -3,8 +3,8 @@
 {%- set source_cols = datavault4dbt.expand_column_list(columns=[link_hashkey, src_rsrc, src_ldts, 'is_active']) -%}
 {%- set final_cols = datavault4dbt.expand_column_list(columns=[link_hashkey, src_rsrc, eff_from_alias, eff_to_alias]) -%}
 
-{%- set end_of_all_times = var('datavault4dbt.end_of_all_times', '8888-12-31T23-59-59') -%}
-{%- set timestamp_format = var('datavault4dbt.timestamp_format', 'YYYY-MM-DDTHH-MI-SS') -%}
+{%- set end_of_all_times = var('datavault4dbt.end_of_all_times', '8888-12-31 23:59:59') -%}
+{%- set timestamp_format = var('datavault4dbt.timestamp_format', 'YYYY-mm-dd HH:MI:SS') -%}
 {%- set is_current_col_alias = var('datavault4dbt.is_current_col_alias', 'IS_CURRENT') -%}
 
 {%- set hash = var('datavault4dbt.hash', 'MD5') -%}
@@ -31,7 +31,7 @@ eff_ranges AS (
         {{ src_rsrc }},
         is_active,
         {{ src_ldts }} AS {{ eff_from_alias }},
-        COALESCE(LAG(ADD_SECONDS({{ src_ldts }}, -0.001)) OVER (PARTITION BY {{ link_hashkey }} ORDER BY {{ src_ldts }} DESC), {{ datavault4dbt.string_to_timestamp( timestamp_format , end_of_all_times) }}) as {{ eff_to_alias }}
+        COALESCE(LAG(ADD_SECONDS({{ src_ldts }}, -0.001)) OVER (PARTITION BY {{ link_hashkey }} ORDER BY {{ src_ldts }} DESC), {{ datavault4dbt.string_to_timestamp( timestamp_format['exasol'] , end_of_all_times['exasol']) }}) as {{ eff_to_alias }}
     FROM source_data
 
 ),
@@ -41,7 +41,7 @@ records_to_select AS (
     SELECT
         {{ datavault4dbt.print_list(final_cols) }}
         {%- if add_is_current_flag %},
-            CASE WHEN {{ eff_to_alias }} = {{ datavault4dbt.string_to_timestamp(timestamp_format, end_of_all_times) }}
+            CASE WHEN {{ eff_to_alias }} = {{ datavault4dbt.string_to_timestamp(timestamp_format['exasol'], end_of_all_times['exasol']) }}
             THEN TRUE
             ELSE FALSE
             END AS {{ is_current_col_alias }}
