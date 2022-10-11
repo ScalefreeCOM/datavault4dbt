8bbe7ecf576a0b260eb0e55c059d3d05064a5280
Barbara Schlottfeldt <101649281+bschlottfeldt@users.noreply.github.com>
2022-10-11 13:04:32 +0200

    Fix issue - only set default dtypes for derived cols if derived cols is set

--- macros/staging/exasol/stage.sql
+++ macros/staging/exasol/stage.sql
@@ -124,10 +124,11 @@
 {%- set final_columns_to_select = final_columns_to_select + source_columns_to_select -%}
 {%- set derived_columns_to_select = datavault4dbt.process_columns_to_select(source_and_derived_column_names, hashed_column_names) | unique | list -%}
 
-{#- Getting Data types for derived columns with detection from source relation -#}
-{%- set derived_columns_with_datatypes = datavault4dbt.derived_columns_datatypes(derived_columns, source_relation) -%}
-{%- set derived_columns_with_datatypes_DICT = fromjson(derived_columns_with_datatypes) -%}
-
+{%- if datavault4dbt.is_something(derived_columns) %}
+  {#- Getting Data types for derived columns with detection from source relation -#}
+  {%- set derived_columns_with_datatypes = datavault4dbt.derived_columns_datatypes(derived_columns, source_relation) -%}
+  {%- set derived_columns_with_datatypes_DICT = fromjson(derived_columns_with_datatypes) -%}
+{%- endif -%}
 {#- Select hashing algorithm -#}
 
 {#- Setting unknown and error keys with default values for the selected hash algorithm -#}
