ade0a67e0c36b575113ce7006e4ecba801ec0028
tkirschke <tkirschke@scalefree.com>
2022-10-17 12:35:35 +0200

    Deleted example models under /macros/

--- macros/models/raw_vault/opportunity_account_l.sql
+++
@@ -1,7 +0,0 @@
-{{ config(schema='public_release_test',
-           materialized='incremental') }}
-
-{{ link(source_model='stage_opportunity',
-        link_hashkey='hk_opportunity_account_l',
-        foreign_hashkeys=['hk_opportunity_h', 'hk_account_h']
-        )}}
