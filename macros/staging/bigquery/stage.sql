bc4b6c7e5cb19ef1eca6f4b4cbc69237a91724a5
Barbara Schlottfeldt <101649281+bschlottfeldt@users.noreply.github.com>
2023-04-27 17:00:08 +0200

    Added macro for hash method and replaced use of var() with the macro call

--- macros/staging/bigquery/stage.sql
+++ macros/staging/bigquery/stage.sql
@@ -136,7 +136,7 @@
 {#- Select hashing algorithm -#}
 
 {#- Setting unknown and error keys with default values for the selected hash algorithm -#}
-{%- set hash = var('datavault4dbt.hash', 'MD5') -%}
+{%- set hash = datavault4dbt.hash_method() -%}
 {%- set hash_dtype = var('datavault4dbt.hash_datatype', 'STRING') -%}
 {%- set hash_default_values = fromjson(datavault4dbt.hash_default_values(hash_function=hash,hash_datatype=hash_dtype)) -%}
 {%- set hash_alg = hash_default_values['hash_alg'] -%}
