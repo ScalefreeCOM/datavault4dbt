ad7662eb9dc6d2d935749b0e478726a3b21d5f09
Tim Kirschke <tkirschke@scalefree.com>
2023-06-02 11:14:05 +0200

    Added QUALIFY and put into conditional. Logic only applied in Snowflake yet

--- macros/tables/bigquery/nh_sat.sql
+++ macros/tables/bigquery/nh_sat.sql
@@ -1,4 +1,4 @@
-{%- macro default__nh_sat(parent_hashkey, src_payload, src_ldts, src_rsrc, source_model) -%}
+{%- macro default__nh_sat(parent_hashkey, src_payload, src_ldts, src_rsrc, source_model, source_is_single_batch) -%}
 
 {%- set beginning_of_all_times = datavault4dbt.beginning_of_all_times() -%}
 {%- set end_of_all_times = datavault4dbt.end_of_all_times() -%}
