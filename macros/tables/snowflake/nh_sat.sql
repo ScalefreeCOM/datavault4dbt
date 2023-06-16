e481409e4ebe287bbcd5a8d1332de1c7fe76e02f
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2023-06-16 16:03:07 +0200

    Added comment

--- macros/tables/snowflake/nh_sat.sql
+++ macros/tables/snowflake/nh_sat.sql
@@ -45,6 +45,8 @@ distinct_hashkeys AS
         {{ this }}    
 ),
 {%- endif %}
+{#- Select all records from the source. If incremental, insert only records, where the
+hashkey is not already in the existing satellite. #}
 
 {#- 
 Select all records from the source. If incremental, insert only records, where the
