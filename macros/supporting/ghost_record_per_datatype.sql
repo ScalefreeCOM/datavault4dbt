d5bc81e85b106a91087793c365252ed67c64fe7d
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2023-06-29 12:07:08 +0200

    Update ghost_record_per_datatype.sql
    Changed "column_name" to "alias"

--- macros/supporting/ghost_record_per_datatype.sql
+++ macros/supporting/ghost_record_per_datatype.sql
@@ -166,7 +166,7 @@
             {%- endif -%}
      {%- elif datatype in ['NUMBER','INT','FLOAT','DECIMAL'] %}0 AS {{ alias }}
      {%- elif datatype == 'BOOLEAN' %}CAST('FALSE' AS BOOLEAN) AS {{ alias }}
-     {%- elif datatype == 'ARRAY' %} CAST('{{ unknown_value__STRING }}' as ARRAY) as {{ column_name }}
+     {%- elif datatype == 'ARRAY' %} CAST('{{ unknown_value__STRING }}' as ARRAY) as "{{ alias }}"
      {%- else %}NULL AS {{ alias }}
      {% endif %}
 {%- elif ghost_record_type == 'error' -%}
@@ -191,7 +191,7 @@
             {%- endif -%}
      {% elif datatype in ['NUMBER','INT','FLOAT','DECIMAL'] %}-1 AS {{ alias }}
      {% elif datatype == 'BOOLEAN' %}CAST('FALSE' AS BOOLEAN) AS {{ alias }}
-     {% elif datatype == 'ARRAY' %} CAST('{{ error_value__STRING }}' as ARRAY) as {{ column_name }}
+     {% elif datatype == 'ARRAY' %} CAST('{{ error_value__STRING }}' as ARRAY) as "{{ alias }}"
      {% else %}NULL AS {{ alias }}
       {% endif %}
 {%- else -%}
