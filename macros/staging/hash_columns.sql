d944b9bd25ecba5103829baf04657ecf9d1e93d5
thoffmann-sf <thoffmann@scalefree.com>
2024-02-05 09:45:32 +0100

    Chenged hash_columns

--- macros/staging/hash_columns.sql
+++ macros/staging/hash_columns.sql
@@ -7,59 +7,6 @@
 {%- endmacro %}
 
 {%- macro default__hash_columns(columns, multi_active_key, main_hashkey_column) -%}
-{%- if columns is mapping and columns is not none -%}
-
-    {%- for col in columns -%}
-
-        {%- if datavault4dbt.is_something(multi_active_key) -%}
-            {% if columns[col] is mapping and columns[col].is_hashdiff -%}
-                {{- datavault4dbt.hash(columns=columns[col]['columns'], 
-                                alias=col, 
-                                is_hashdiff=columns[col]['is_hashdiff'],
-                                multi_active_key=multi_active_key,
-                                main_hashkey_column=main_hashkey_column) -}}
-
-            {{- ", \n" if not loop.last -}}
-
-            {%- elif columns[col] is not mapping and (col|upper) == (main_hashkey_column | upper) -%}
-                {{- datavault4dbt.hash(columns=columns[col],
-                                alias=col,
-                                is_hashdiff=false) -}}  
-            {{- ", \n" if not loop.last -}}
-            {%- endif -%}
-
-
-        {%- else -%}          
-
-            {% if columns[col] is mapping and columns[col].is_hashdiff -%}
-                {%- if columns[col].use_rtrim -%}
-                    {%- set rtrim_hashdiff = true -%}
-                {%- else -%}
-                    {%- set rtrim_hashdiff = false -%}
-                {%- endif -%}
-                {{- datavault4dbt.hash(columns=columns[col]['columns'], 
-                                alias=col, 
-                                is_hashdiff=columns[col]['is_hashdiff'],
-                                rtrim_hashdiff=rtrim_hashdiff) -}}
-            {%- elif columns[col] is not mapping -%}
-                {{- datavault4dbt.hash(columns=columns[col],
-                                alias=col,
-                                is_hashdiff=false) -}}
-            
-            {%- elif columns[col] is mapping and not columns[col].is_hashdiff -%}
-                {%- if execute -%}
-                    {%- do exceptions.warn("[" ~ this ~ "] Warning: You provided a list of columns under a 'columns' key, but did not provide the 'is_hashdiff' flag. Use list syntax for PKs.") -%}
-                {% endif %}
-                {{- datavault4dbt.hash(columns=columns[col]['columns'], alias=col) -}}
-            {%- endif -%}
-        {{- ",\n" if not loop.last -}}
-        {%- endif -%}
-        
-    {%- endfor -%}
-{%- endif %}
-{%- endmacro -%}
-
-{%- macro snowflake__hash_columns(columns, multi_active_key, main_hashkey_column) -%}
 
 {%- if columns is mapping and columns is not none -%}
 
@@ -124,5 +71,58 @@
 
 {%- endif %}
 
+{%- endmacro -%}
+
+{%- macro redshift__hash_columns(columns, multi_active_key, main_hashkey_column) -%}
+{%- if columns is mapping and columns is not none -%}
+
+    {%- for col in columns -%}
+
+        {%- if datavault4dbt.is_something(multi_active_key) -%}
+            {% if columns[col] is mapping and columns[col].is_hashdiff -%}
+                {{- datavault4dbt.hash(columns=columns[col]['columns'], 
+                                alias=col, 
+                                is_hashdiff=columns[col]['is_hashdiff'],
+                                multi_active_key=multi_active_key,
+                                main_hashkey_column=main_hashkey_column) -}}
+
+            {{- ", \n" if not loop.last -}}
+
+            {%- elif columns[col] is not mapping and (col|upper) == (main_hashkey_column | upper) -%}
+                {{- datavault4dbt.hash(columns=columns[col],
+                                alias=col,
+                                is_hashdiff=false) -}}  
+            {{- ", \n" if not loop.last -}}
+            {%- endif -%}
+
+
+        {%- else -%}          
+
+            {% if columns[col] is mapping and columns[col].is_hashdiff -%}
+                {%- if columns[col].use_rtrim -%}
+                    {%- set rtrim_hashdiff = true -%}
+                {%- else -%}
+                    {%- set rtrim_hashdiff = false -%}
+                {%- endif -%}
+                {{- datavault4dbt.hash(columns=columns[col]['columns'], 
+                                alias=col, 
+                                is_hashdiff=columns[col]['is_hashdiff'],
+                                rtrim_hashdiff=rtrim_hashdiff) -}}
+            {%- elif columns[col] is not mapping -%}
+                {{- datavault4dbt.hash(columns=columns[col],
+                                alias=col,
+                                is_hashdiff=false) -}}
+            
+            {%- elif columns[col] is mapping and not columns[col].is_hashdiff -%}
+                {%- if execute -%}
+                    {%- do exceptions.warn("[" ~ this ~ "] Warning: You provided a list of columns under a 'columns' key, but did not provide the 'is_hashdiff' flag. Use list syntax for PKs.") -%}
+                {% endif %}
+                {{- datavault4dbt.hash(columns=columns[col]['columns'], alias=col) -}}
+            {%- endif -%}
+        {{- ",\n" if not loop.last -}}
+        {%- endif -%}
+        
+    {%- endfor -%}
+{%- endif %}
 {%- endmacro -%}
 
