5a10b8b78aed8fa1b51f9b208d468622b1073094
tkiehn <162969167+tkiehn@users.noreply.github.com>
2024-05-13 10:25:30 +0200

    change hardcoded hash datatype defaults to varchar(32) for redshift

--- macros/supporting/hash.sql
+++ macros/supporting/hash.sql
@@ -306,7 +306,7 @@
 {%- set hashdiff_input_case_sensitive = var('datavault4dbt.hashdiff_input_case_sensitive', TRUE) -%}
 
 {#- Select hashing algorithm -#}
-{%- set hash_dtype = var('datavault4dbt.hash_datatype', 'VARCHAR') -%}
+{%- set hash_dtype = var('datavault4dbt.hash_datatype', 'VARCHAR(32)') -%}
 {{ log('hash type in hash macro: ' ~ hash_dtype, false) }}
 {%- set hash_default_values = fromjson(datavault4dbt.hash_default_values(hash_function=hash,hash_datatype=hash_dtype)) -%}
 {%- set hash_alg = hash_default_values['hash_alg'] -%}
