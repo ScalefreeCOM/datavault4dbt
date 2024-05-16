6fd6e0525ef10c744779fc6b8844d298036ded11
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2024-05-16 11:59:09 +0200

    Re-Added upper() mapping to exclude columns

--- macros/internal/helpers/stage_processing_macros.sql
+++ macros/internal/helpers/stage_processing_macros.sql
@@ -1,5 +1,5 @@
 {%- macro process_columns_to_select(columns_list=none, exclude_columns_list=none) -%}
-    {# {% set exclude_columns_list = exclude_columns_list | map('upper') | list %} #}
+    {% set exclude_columns_list = exclude_columns_list | map('upper') | list %}
     {% set columns_to_select = [] %}
 
     {% if not datavault4dbt.is_list(columns_list) or not datavault4dbt.is_list(exclude_columns_list)  %}
