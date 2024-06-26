a11fe41827a78ecb10e17f29aa1f6d33b407eb26
tkiehn <162969167+tkiehn@users.noreply.github.com>
2024-06-26 16:00:20 +0200

    Update stage.sql
    If ldts is none or empty it is populated with the current timestamp

--- macros/staging/stage.sql
+++ macros/staging/stage.sql
@@ -108,6 +108,11 @@
     {%- if include_source_columns is none or include_source_columns == "" -%}
       {%- set include_source_columns = true -%}
     {%- endif -%}
+    
+    {# If ldts is empty replace it with the current timestamp #}
+    {%- if datavault4dbt.is_nothing(ldts) == True -%}
+      {%- set ldts = datavault4dbt.current_timestamp() -%}
+    {%- endif -%}
 
     {{- adapter.dispatch('stage', 'datavault4dbt')(include_source_columns=include_source_columns,
                                         ldts=ldts,
