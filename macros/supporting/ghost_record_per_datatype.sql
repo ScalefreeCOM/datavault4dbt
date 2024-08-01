586b69369a7c4c1043fe0392b312ba5a2a6ddbae
Tim Kirschke <81677440+tkirschke@users.noreply.github.com>
2024-08-01 15:58:49 +0200

    Fixed Redshift Ghost Records for Numerical Values

--- macros/supporting/ghost_record_per_datatype.sql
+++ macros/supporting/ghost_record_per_datatype.sql
@@ -366,12 +366,8 @@
         {%- elif datatype == 'DATE'-%} TO_DATE('{{ beginning_of_all_times_date }}', '{{ date_format }}' ) as "{{ alias }}"
         {%- elif datatype == 'VARCHAR' or datatype == 'CHARACTER VARYING' or datatype == 'NVARCHAR' or datatype == 'TEXT' %} '{{unknown_value__STRING}}' as {{ alias }}
         {%- elif datatype == 'CHAR' or datatype == 'CHARACTER' or datatype == 'NCHAR' or datatype == 'BPCHAR' %} '{{unknown_value__STRING}}' as {{ alias }}
-        {%- elif datatype == 'INTEGER' or datatype == 'INT' or datatype == 'INT4' %} CAST('0' as INT) as {{ alias }}
-        {%- elif datatype == 'SMALLINT' or datatype == 'INT2' %} CAST('0' as INT2) as {{ alias }}
-        {%- elif datatype == 'BIGINT' or datatype == 'INT8' %} CAST('0' as INT8) as {{ alias }}
+        {%- elif datatype in ['INTEGER', 'INT', 'INT2', 'INT4', 'INT8', 'SMALLINT', 'BIGINT', 'REAL', 'FLOAT4', 'DOUBLE PRECISION', 'FLOAT', 'FLOAT8'] %} 0 as {{ alias }}
         {%- elif 'DECIMAL' in datatype or 'NUMERIC' in datatype %} CAST('0' as {{ datatype }}) as {{ alias }}
-        {%- elif datatype == 'REAL' or datatype == 'FLOAT4' %} CAST('0' as FLOAT4) as {{ alias }}
-        {%- elif datatype == 'DOUBLE PRECISION' or datatype == 'FLOAT' or datatype == 'FLOAT8' %} CAST('0' as FLOAT) as {{ alias }}
         {%- elif datatype == 'BOOLEAN' or datatype == 'BOOL' %} CAST('TRUE' as BOOLEAN) as {{ alias }}
         {%- elif datatype == 'VARBYTE' or datatype == 'VARBINARY' or datatype == 'BINARY VARYING' %} CAST('{{ unknown_value__HASHTYPE }}' as {{ datatype }}) as "{{ alias }}"
         {%- else %} CAST(NULL as {{ datatype }}) as {{ alias }}
@@ -383,12 +379,8 @@
         {%- elif datatype == 'DATE'-%} TO_DATE('{{ end_of_all_times_date }}', '{{ date_format }}' ) as "{{ alias }}"
         {%- elif datatype == 'VARCHAR' or datatype == 'CHARACTER VARYING' or datatype == 'NVARCHAR' or datatype == 'TEXT' %} '{{error_value__STRING}}' as {{ alias }}
         {%- elif datatype == 'CHAR' or datatype == 'CHARACTER' or datatype == 'NCHAR' or datatype == 'BPCHAR' %} '{{error_value__STRING}}' as {{ alias }}
-        {%- elif datatype == 'INTEGER' or datatype == 'INT' or datatype == 'INT4' %} CAST('-1' as INT) as {{ alias }}
-        {%- elif datatype == 'SMALLINT' or datatype == 'INT2' %} CAST('-1' as INT2) as {{ alias }}
-        {%- elif datatype == 'BIGINT' or datatype == 'INT8' %} CAST('-1' as INT8) as {{ alias }}
+        {%- elif datatype in ['INTEGER', 'INT', 'INT2', 'INT4', 'INT8', 'SMALLINT', 'BIGINT', 'REAL', 'FLOAT4', 'DOUBLE PRECISION', 'FLOAT', 'FLOAT8'] %} -1 as {{ alias }}
         {%- elif 'DECIMAL' in datatype or 'NUMERIC' in datatype %} CAST('-1' as {{ datatype }}) as {{ alias }}
-        {%- elif datatype == 'REAL' or datatype == 'FLOAT4' %} CAST('-1' as FLOAT4) as {{ alias }}
-        {%- elif datatype == 'DOUBLE PRECISION' or datatype == 'FLOAT' or datatype == 'FLOAT8' %} CAST('-1' as FLOAT) as {{ alias }}
         {%- elif datatype == 'BOOLEAN' or datatype == 'BOOL' %} CAST('FALSE' as BOOLEAN) as {{ alias }}
         {%- elif datatype == 'VARBYTE' or datatype == 'VARBINARY' or datatype == 'BINARY VARYING' %} CAST('{{ error_value__HASHTYPE }}' as {{ datatype }}) as "{{ alias }}"
         {%- else %} CAST(NULL as {{ datatype }}) as {{ alias }}
