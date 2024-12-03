956e71abdbba5b8fbdeb2ce5e3cf4adce406a14c
Theo Kiehn <162969167+tkiehn@users.noreply.github.com>
2024-12-03 10:34:33 +0100

    Update yaml_metadata_parser.sql, fix if-nesting, rename metadata_dict

--- macros/internal/metadata_processing/yaml_metadata_parser.sql
+++ macros/internal/metadata_processing/yaml_metadata_parser.sql
@@ -1,16 +1,12 @@
 {% macro yaml_metadata_parser(name=none, yaml_metadata=none, parameter=none, required=False, documentation=none) %}
 
     {% if datavault4dbt.is_something(yaml_metadata) %}
-        {%- set yaml_metadata = fromyaml(yaml_metadata) -%}
-        {% if name in yaml_metadata.keys() %}
-            {% set return_value = yaml_metadata.get(name) %}
+        {%- set metadata_dict = fromyaml(yaml_metadata) -%}
+        {% if name in metadata_dict.keys() %}
+            {% set return_value = metadata_dict.get(name) %}
         {% elif datavault4dbt.is_something(parameter) %}
             {% set return_value = parameter %}
-
-            {% if required %}
-                {% do exceptions.warn("[" ~ this ~ "] Warning: yaml-metadata given, but parameter '" ~ name ~ "' not defined in there. Using '" ~ name ~ "' parameter defined outside. We advise to use only one method of parameter passing.") %}
-            {% else %}
-
+            {% do exceptions.warn("[" ~ this ~ "] Warning: yaml-metadata given, but parameter '" ~ name ~ "' not defined in there. Using '" ~ name ~ "' parameter defined outside. We advise to use only one method of parameter passing.") %}
         {% elif required %}
             {{ exceptions.raise_compiler_error("[" ~ this ~ "] Error: yaml-metadata given, but required parameter '" ~ name ~ "' not defined in there or outside in the parameter. \n Description of parameter '" ~ name ~ "': \n" ~ documentation ) }}
         {% endif %}
