db5d924506fbe114d784f46086a4d2511724c16a
tkiehn <162969167+tkiehn@users.noreply.github.com>
2025-02-11 09:12:47 +0100

    exasol__attribute_standardise, remove unnecessary escape character

--- macros/supporting/hash_standardization.sql
+++ macros/supporting/hash_standardization.sql
@@ -14,7 +14,7 @@ CONCAT('\"', REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(TRIM(CAST([EXPRESSION] AS STR
 {%- set quote = var('quote', '"') -%}
 {%- set null_placeholder_string = var('null_placeholder_string', '^^') -%}
 
-CONCAT('"', REPLACE(REPLACE(REPLACE(TRIM(CAST([EXPRESSION] AS VARCHAR(20000) UTF8 )), '\\\', '\\\\\'), '[QUOTE]', '"'), '[NULL_PLACEHOLDER_STRING]', '--'), '\"')
+CONCAT('"', REPLACE(REPLACE(REPLACE(TRIM(CAST([EXPRESSION] AS VARCHAR(20000) UTF8 )), '\\\', '\\\\\'), '[QUOTE]', '"'), '[NULL_PLACEHOLDER_STRING]', '--'), '"')
 
 {%- endmacro -%}
 
