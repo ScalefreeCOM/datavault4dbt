c3a38d7f444cf5f5b7cc5866004c09973118009b
Tim Kirschke <tkirschke@scalefree.com>
2025-02-28 16:52:34 +0100

    Working Macro for single link rehash

--- macros/upgrade_to_v2/single_entities/rehash_single_link.sql
+++ macros/upgrade_to_v2/single_entities/rehash_single_link.sql
@@ -9,15 +9,19 @@ hub_config:
       business_keys:
         - n_nationkey
 
+Example usage: 
+
+dbt run-operation rehash_single_link --args '{link: customer_nation_l, link_hashkey: hk_customer_nation_l, overwrite_hash_values: true, hub_config: [{hub_hashkey: hk_customer_h, hub_name: customer_h, business_keys: [c_custkey]}, {hub_hashkey: hk_nation_h, hub_name: nation_h, business_keys: [n_nationkey]}]}'
+
 #}
 
 
 
-{% macro rehash_single_link(link, link_hashkey, hub_config, additional_hash_input_cols, overwrite_hash_values=false, output_logs=true) %}
+{% macro rehash_single_link(link, link_hashkey, hub_config, additional_hash_input_cols=[], overwrite_hash_values=false, output_logs=true) %}
 
     {% set new_link_hashkey_name = link_hashkey ~ '_new' %}
     {% set hash_datatype = var('datavault4dbt.hash_datatype', 'STRING') %}
-    {% set ns = namespace(hub_hashkeys=[], new_hash_columns=[{"name": new_link_hashkey_name, "data_type": hash_datatype}]) %}
+    {% set ns = namespace(hub_hashkeys=[], new_hash_columns=[{"name": new_link_hashkey_name, "data_type": hash_datatype}], columns_to_drop=[{"name": link_hashkey + '_deprecated'}]) %}
 
     {% set link_relation = ref(link) %}
 
@@ -29,6 +33,7 @@ hub_config:
         {% set hub_hashkey_dict = {
             "current_hashkey_name": hub.hub_hashkey,
             "new_hashkey_name": new_hub_hashkey_name,
+            "hub_name": hub.hub_name,
             "hub_join_alias": hub_join_alias,
             "prefixed_business_keys": prefixed_business_keys
         } %}
@@ -49,8 +54,109 @@ hub_config:
     {{ alter_relation_add_remove_columns(relation=link_relation, add_columns=ns.new_hash_columns) }}
     {{ log('ALTER TABLE statement completed!', output_logs) }}
 
-    
+    {# generating the UPDATE statement that populates the new columns. #}
+    {% set update_sql = datavault4dbt.link_update_statement(link_relation=link_relation,
+                                                hub_hashkeys=ns.hub_hashkeys,
+                                                link_hashkey=link_hashkey,
+                                                new_link_hashkey_name=new_link_hashkey_name,
+                                                additional_hash_input_cols=additional_hash_input_cols) %}
+
+    {# Executing the UPDATE statement. #}
+    {{ log('Executing UPDATE statement...' ~ update_sql, output_logs) }}
+    {% do run_query(update_sql) %}
+    {{ log('UPDATE statement completed!', output_logs) }}
+
+    {# renaming existing hash columns #}
+    {% if overwrite_hash_values %}
+        {{ log('Replacing existing hash values with new ones...', output_logs) }}
+
+        {% set overwrite_sql %}
+        {# Rename Link Hashkey #}
+            {{ get_rename_column_sql(relation=link_relation, old_col_name=link_hashkey, new_col_name=link_hashkey + '_deprecated') }}
+            {{ get_rename_column_sql(relation=link_relation, old_col_name=new_link_hashkey_name, new_col_name=link_hashkey) }}
+            
+            {# Rename All Hub Hashkeys #}
+            {% for hub_hashkey in ns.hub_hashkeys %}
+                {{ get_rename_column_sql(relation=link_relation, old_col_name=hub_hashkey.current_hashkey_name, new_col_name=hub_hashkey.current_hashkey_name + '_deprecated') }}
+                {{ get_rename_column_sql(relation=link_relation, old_col_name=hub_hashkey.new_hashkey_name, new_col_name=hub_hashkey.current_hashkey_name) }}
+
+                {# Prepare list of 'deprecated' hub hashkey columns to drop them later on. #}
+                {% do ns.columns_to_drop.append({"name": hub_hashkey.current_hashkey_name + '_deprecated'}) %}
+
+            {% endfor %}
+
+        {% endset %}
+
+        {% do run_query(overwrite_sql) %}
+        
+        {{ alter_relation_add_remove_columns(relation=link_relation, remove_columns=ns.columns_to_drop) }}
+        
+        {{ log('Existing Hash values overwritten!', output_logs) }}
+
+    {% endif %}
+
+{% endmacro %}
 
 
+{% macro link_update_statement(link_relation, hub_hashkeys, link_hashkey, new_link_hashkey_name, additional_hash_input_cols=[]) %}
+
+    {{ adapter.dispatch('link_update_statement', 'datavault4dbt')(link_relation=link_relation,
+                                                                hub_hashkeys=hub_hashkeys,
+                                                                link_hashkey=link_hashkey,
+                                                                new_link_hashkey_name=new_link_hashkey_name,
+                                                                additional_hash_input_cols=additional_hash_input_cols) }}
+
+{% endmacro %}
+
+
+{% macro default__link_update_statement(link_relation, hub_hashkeys, link_hashkey, new_link_hashkey_name, additional_hash_input_cols) %}
+
+    {% set ns = namespace(link_hashkey_input_cols=[], hash_config_dict={}, update_sql_part1='', update_sql_part2='') %}
+
+    {% set ns.update_sql_part1 %}
+    UPDATE {{ link_relation }} link
+    SET 
+        {{ new_link_hashkey_name }} = nh.{{ new_link_hashkey_name }}
+
+    {% endset %}
+
+        {% for hub_hashkey in hub_hashkeys %}
+            
+            {% set ns.update_sql_part1 = ns.update_sql_part1 + '\n,' + hub_hashkey.new_hashkey_name + ' = nh.' + hub_hashkey.new_hashkey_name %}
+            
+            {% do ns.hash_config_dict.update({hub_hashkey.new_hashkey_name: hub_hashkey.prefixed_business_keys}) %}
+            {% set ns.link_hashkey_input_cols = ns.link_hashkey_input_cols + hub_hashkey.prefixed_business_keys %}
+        
+        {% endfor %}
+
+        {# Defining Hash config for new link hashkey. #}
+        {% set ns.link_hashkey_input_cols = ns.link_hashkey_input_cols + additional_hash_input_cols %}
+        {% do ns.hash_config_dict.update({new_link_hashkey_name: ns.link_hashkey_input_cols}) %}
+
+    {{ log('hash_config: ' ~ ns.hash_config_dict, true)}}
+
+    {% set ns.update_sql_part2 %}
+    FROM (
+
+        SELECT
+            link.{{ link_hashkey }},
+            {{ datavault4dbt.hash_columns(columns=ns.hash_config_dict) }}
+        FROM {{ link_relation }} link
+    {% endset %}
+        
+        {% for hub in hub_hashkeys %}
+
+            {% set ns.update_sql_part2 = ns.update_sql_part2 + '\n LEFT JOIN ' + ref(hub.hub_name).render() + ' ' + hub.hub_join_alias + '\n    ON link.' + hub.current_hashkey_name + ' = ' + hub.hub_join_alias + '.' + hub.current_hashkey_name %}
+
+        {% endfor %}
+
+    {% set update_sql_part3 %}
+    ) nh
+    WHERE link.{{ link_hashkey }} = nh.{{ link_hashkey }}
+    {% endset %}
+
+    {% set update_sql = ns.update_sql_part1 + ns.update_sql_part2 + update_sql_part3 %}
+
+    {{ return(update_sql) }}
 
 {% endmacro %}
