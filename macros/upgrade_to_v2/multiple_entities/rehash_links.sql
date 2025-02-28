ccb18a47c5c0eabb67c2d6ad57d99a61761856cf
Tim Kirschke <tkirschke@scalefree.com>
2025-02-28 17:00:32 +0100

    Macro to hash multiple links finished

---
+++ macros/upgrade_to_v2/multiple_entities/rehash_links.sql
@@ -0,0 +1,22 @@
+
+
+{% macro rehash_links(link_yaml) %}
+
+    {% set link_dict = fromyaml(link_yaml) %}
+
+    {% set overwrite_hash_values = link_dict.config.get('overwrite_hash_values', false) %}
+
+    {% for link in link_dict.get('links') %}
+        {% set specific_link_overwrite_hash = link.get('overwrite_hash_values', overwrite_hash_values) %}
+         
+        {% do datavault4dbt.rehash_single_link(link=link.name, 
+                                       link_hashkey=link.link_hashkey,
+                                       additional_hash_input_cols=link.additional_hash_input_cols,
+                                       overwrite_hash_values=specific_link_overwrite_hash,
+                                       hub_config=link.hub_config,
+                                       output_logs=true) %}
+                        
+        {{ log(link.name ~ ' rehashed successfully.', true) }}                
+    {% endfor %}
+
+{% endmacro %}
