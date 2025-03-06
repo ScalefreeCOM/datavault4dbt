656708f026b94adb0129197686c3dde5d47ffbe9
umattern <Umattern@scalefree.com>
2025-03-06 11:30:45 +0100

    POSTGRES.control_snap_v0: added is_eom, is_quarterly, is_eoq, is_eoy

--- macros/tables/postgres/control_snap_v0.sql
+++ macros/tables/postgres/control_snap_v0.sql
@@ -49,10 +49,26 @@ enriched_timestamps AS (
             WHEN EXTRACT(DAY FROM sdts) = 1 THEN TRUE
             ELSE FALSE
         END as is_monthly,
+        CASE 
+            WHEN EXTRACT(DAY FROM (sdts + INTERVAL '1 day')) = 1 THEN TRUE
+            ELSE FALSE
+        END AS is_end_of_month,
+        CASE 
+            WHEN EXTRACT(MONTH FROM sdts) IN (1, 4, 7, 10) AND EXTRACT(DAY FROM sdts) = 1 THEN TRUE
+            ELSE FALSE
+        END AS is_quarterly,
+        CASE 
+            WHEN EXTRACT(MONTH FROM sdts) IN (3, 6, 9, 12) AND EXTRACT(DAY FROM (sdts + INTERVAL '1 day')) = 1 THEN TRUE
+            ELSE FALSE
+        END AS is_end_of_quarter, 
         CASE
             WHEN EXTRACT(DAY FROM sdts) = 1 AND EXTRACT(MONTH FROM sdts) = 1 THEN TRUE
             ELSE FALSE
         END as is_yearly,
+        CASE 
+            WHEN EXTRACT(MONTH FROM sdts) = 12 AND EXTRACT(DAY FROM sdts) = 31 THEN TRUE
+            ELSE FALSE
+        END AS is_end_of_year,
         NULL as comment
     FROM initial_timestamps
 
