5ad279b65513d89a545189bcc4767d9643b15839
Tim Kirschke <tkirschke@scalefree.com>
2025-03-31 11:11:09 +1100

    Added specific overwrite macros

---
+++ macros/upgrade_to_v2/internal_overwrites/alter_table_add_col.sql
@@ -0,0 +1,41 @@
+{% macro alter_relation_add_remove_columns(relation, add_columns, remove_columns) %}
+{{ adapter.dispatch('alter_relation_add_remove_columns', 'datavault4dbt')(relation=relation, add_columns=add_columns, remove_columns=remove_columns) }}
+{% endmacro %}
+
+{% macro snowflake__alter_relation_add_remove_columns(relation, add_columns, remove_columns) %}
+
+    {% if relation.is_dynamic_table -%}
+        {% set relation_type = "dynamic table" %}
+    {% else -%}
+        {% set relation_type = "Table" %}
+    {% endif %}
+
+    {% if add_columns %}
+
+    {% set sql -%}
+       alter {{ relation.get_ddl_prefix_for_alter() }} {{ relation_type }} {{ relation.render() }} add column
+          {% for column in add_columns %}
+            {{ column.name }} {{ column.data_type }}{{ ',' if not loop.last }}
+          {% endfor %}
+    {%- endset -%}
+
+     {{ log('alter sql: ' ~ sql, false)}}
+
+    {% do run_query(sql) %}
+
+    {% endif %}
+
+    {% if remove_columns %}
+
+    {% set sql -%}
+        alter {{ relation.get_ddl_prefix_for_alter() }} {{ relation_type }} {{ relation.render() }} drop column
+            {% for column in remove_columns %}
+                {{ column.name }}{{ ',' if not loop.last }}
+            {% endfor %}
+    {%- endset -%}
+
+    {% do run_query(sql) %}
+
+    {% endif %}
+
+{% endmacro %}
---
+++ macros/upgrade_to_v2/internal_overwrites/get_rename_column_sql.sql
@@ -0,0 +1,35 @@
+{% macro get_rename_column_sql(relation, old_col_name, new_col_name) %}
+{{ adapter.dispatch('get_rename_column_sql', 'datavault4dbt')(relation=relation, old_col_name=old_col_name, new_col_name=new_col_name) }}
+{% endmacro %}
+
+{% macro default__get_rename_column_sql(relation, old_col_name, new_col_name) %}
+
+    {% if relation.is_dynamic_table -%}
+        {% set relation_type = "dynamic table" %}
+    {% else -%}
+        {% set relation_type = "Table" %}
+    {% endif %}
+
+    {% set query %}
+    alter {{ relation.get_ddl_prefix_for_alter() }} {{ relation_type }} {{ relation.render() }} rename column {{ old_col_name }} to {{ new_col_name }};
+    {% endset %}
+
+    {{ return(query) }}
+
+{% endmacro %}
+
+{% macro snowflake__get_rename_column_sql(relation, old_col_name, new_col_name) %}
+
+    {% if relation.is_dynamic_table -%}
+        {% set relation_type = "dynamic table" %}
+    {% else -%}
+        {% set relation_type = "Table" %}
+    {% endif %}
+
+    {% set query %}
+    alter {{ relation.get_ddl_prefix_for_alter() }} {{ relation_type }} {{ relation.render() }} rename column {{ old_col_name }} to {{ new_col_name }};
+    {% endset %}
+
+    {{ return(query) }}
+
+{% endmacro %}
