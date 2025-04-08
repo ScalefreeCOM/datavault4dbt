77199756a597218743b4461caf6a865b9c37beab
Tim Kirschke <tkirschke@scalefree.com>
2025-04-08 14:31:55 +0200

    added redshift specific hash_default_values to support SHA256

--- macros/supporting/hash_default_values.sql
+++ macros/supporting/hash_default_values.sql
@@ -204,3 +204,31 @@
     {{ return(dict_result | tojson ) }}
 
 {%- endmacro -%}
+
+
+{%- macro redshift__hash_default_values(hash_function, hash_datatype) -%}
+
+    {%- set dict_result = {} -%}
+    {%- set hash_alg = '' -%}
+    {%- set unknown_key = '' -%}
+    {%- set error_key = '' -%}
+
+    {%- if hash_function == 'MD5' -%}
+        {%- set hash_alg = 'MD5' -%}
+        {%- set unknown_key = '!00000000000000000000000000000000' -%}
+        {%- set error_key = '!ffffffffffffffffffffffffffffffff' -%}
+    {%- elif hash_function == 'SHA' or hash_function == 'SHA1' -%}
+        {%- set hash_alg = 'SHA1' -%}
+        {%- set unknown_key = '!0000000000000000000000000000000000000000' -%}
+        {%- set error_key = '!ffffffffffffffffffffffffffffffffffffffff' -%}
+    {%- elif hash_function == 'SHA2' or hash_function == 'SHA256' -%}
+        {%- set hash_alg = 'SHA2' -%}
+        {%- set unknown_key = '!0000000000000000000000000000000000000000000000000000000000000000' -%}
+        {%- set error_key = '!ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff' -%}
+    {%- endif -%}
+
+    {%- do dict_result.update({"hash_alg": hash_alg, "unknown_key": unknown_key, "error_key": error_key }) -%}
+
+    {{ return(dict_result | tojson ) }}
+
+{%- endmacro -%}
