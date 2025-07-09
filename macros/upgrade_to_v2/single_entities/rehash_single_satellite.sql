910e5bad0f831834e173ca1123f204d5828a9b8a
jstendel <joshua.stendel@gmail.com>
2025-07-09 08:43:13 +0200

    Added top-level macro for remaining single entities

--- macros/upgrade_to_v2/single_entities/rehash_single_satellite.sql
+++ macros/upgrade_to_v2/single_entities/rehash_single_satellite.sql
@@ -5,92 +5,15 @@
 
 {% macro rehash_single_satellite(satellite, hashkey, hashdiff, payload, parent_entity, business_keys=none, overwrite_hash_values=false, output_logs=true, drop_old_values=true) %}
 
-    {% set satellite_relation = ref(satellite) %}
-    {% set parent_relation = ref(parent_entity) %}
-
-    {% set ldts_col = var(datavault4dbt.ldts_alias, 'ldts') %}
-
-    {% set new_hashkey_name = hashkey + '_new' %}
-    {% set new_hashdiff_name = hashdiff + '_new' %}
-
-    {% set hash_datatype = var('datavault4dbt.hash_datatype', 'STRING') %}
-
-    {# Create definition of new columns for ALTER statement. #}
-    {% set new_hash_columns = [
-        {"name": new_hashkey_name,
-         "data_type": hash_datatype},
-        {"name": new_hashdiff_name, 
-         "data_type": hash_datatype}
-    ]%}
-
-    {# ALTER existing satellite to add new hashkey and new hashdiff. #}
-    {{ log('Executing ALTER TABLE statement...', output_logs) }}
-    {{ alter_relation_add_remove_columns(relation=satellite_relation, add_columns=new_hash_columns) }}
-    {{ log('ALTER TABLE statement completed!', output_logs) }}
-
-    {% if datavault4dbt.is_something(business_keys) %}
-        {# Ensuring business_keys is a list. #}
-        {% if business_keys is iterable and business_keys is not string %}
-            {% set business_key_list = business_keys %}
-        {% else %}
-            {% set business_key_list = [business_keys] %}
-        {% endif %}
-
-        {# Adding prefixes to column names for proper selection. #}
-        {% set prefixed_payload = datavault4dbt.prefix(columns=payload, prefix_str='sat').split(',') %}
-        {% set prefixed_business_keys = datavault4dbt.prefix(columns=business_key_list, prefix_str='parent').split(',') %}
-
-        {% set hash_config_dict = {new_hashkey_name: prefixed_business_keys, new_hashdiff_name: prefixed_payload} %}
-        
-    {% else %}
-
-        {# Adding prefixes to column names for proper selection. #}
-        {% set prefixed_payload = datavault4dbt.prefix(columns=payload, prefix_str='sat').split(',') %}
-
-        {% set hash_config_dict = {new_hashdiff_name: prefixed_payload} %}
-
-    {% endif %}
-
-    {# generating the UPDATE statement that populates the new columns. #}
-    {% set update_sql = datavault4dbt.satellite_update_statement(satellite_relation=satellite_relation,
-                                                new_hashkey_name=new_hashkey_name,
-                                                new_hashdiff_name=new_hashdiff_name,
-                                                hashkey=hashkey, 
-                                                ldts_col=ldts_col,
-                                                hash_config_dict=hash_config_dict,
-                                                parent_relation=parent_relation) %}
-
-    {# Executing the UPDATE statement. #}
-    {{ log('Executing UPDATE statement...', output_logs) }}
-    {{ '/* UPDATE STATEMENT FOR ' ~ satellite ~ '\n' ~ update_sql ~ '*/' }}
-    {% do run_query(update_sql) %}
-    {{ log('UPDATE statement completed!', output_logs) }}
-
-    {% set columns_to_drop = [
-        {"name": hashkey + '_deprecated'},
-        {"name": hashdiff + '_deprecated'}
-    ]%}
-    {# renaming existing hash columns #}
-    {% if overwrite_hash_values %}
-        {{ log('Replacing existing hash values with new ones...', output_logs) }}
-
-        {% set overwrite_sql %}
-        {{ datavault4dbt.get_rename_column_sql(relation=satellite_relation, old_col_name=hashkey, new_col_name=hashkey + '_deprecated') }}
-        {{ datavault4dbt.get_rename_column_sql(relation=satellite_relation, old_col_name=hashdiff, new_col_name=hashdiff + '_deprecated') }}
-        {{ datavault4dbt.get_rename_column_sql(relation=satellite_relation, old_col_name=new_hashkey_name, new_col_name=hashkey) }}
-        {{ datavault4dbt.get_rename_column_sql(relation=satellite_relation, old_col_name=new_hashdiff_name, new_col_name=hashdiff ) }}
-        {% endset %}
-
-        {% do run_query(overwrite_sql) %}
-        
-        {% if drop_old_values %}
-            {{ alter_relation_add_remove_columns(relation=satellite_relation, remove_columns=columns_to_drop) }}
-            {{ log('Existing Hash values overwritten!', output_logs) }}
-        {% endif %}
-
-    {% endif %}
-
-    {{ return(columns_to_drop) }}
+    {{ adapter.dispatch('rehash_single_ma_satellite', 'datavault4dbt')(satellite=satellite,
+                                                                        hashkey=hashkey,
+                                                                        hashdiff=hashdiff,
+                                                                        payload=payload,
+                                                                        parent_entity=parent_entity,
+                                                                        business_keys=business_keys,
+                                                                        overwrite_hash_values=overwrite_hash_values,
+                                                                        output_logs=output_logs,
+                                                                        drop_old_values=drop_old_values)}}
 
 {% endmacro %}
 
@@ -104,63 +27,5 @@
                                                                        ldts_col=ldts_col,
                                                                        hash_config_dict=hash_config_dict,
                                                                        parent_relation=parent_relation) }}
-
-{% endmacro %}
-
-
-{% macro default__satellite_update_statement(satellite_relation, new_hashkey_name, new_hashdiff_name, hashkey, ldts_col, hash_config_dict, parent_relation) %}
-
-    {% set ns = namespace(parent_already_rehashed=false) %}
-
-    {#
-        If parent entity is rehashed already (via rehash_all_rdv_entities macro), the "_deprecated"
-        hashkey column needs to be used for joining, and the regular hashkey should be selected. 
-
-        Otherwise, the regular hashkey should be used for joining. 
-    #}
-    {% set all_parent_columns = adapter.get_columns_in_relation(parent_relation) %}
-    {% for column in all_parent_columns %}
-        {{ log('parent column names: ' ~ all_parent_columns, true) }}
-        {% if column.name|lower == hashkey|lower + '_deprecated' %}
-            {% set ns.parent_already_rehashed = true %}
-            {{ log('parent_already hashed set to true for ' ~ satellite_relation.name, true) }}
-        {% endif %}
-    {% endfor %}
-
-    {% if ns.parent_already_rehashed %}
-        {% set join_hashkey_col = hashkey + '_deprecated' %}
-        {% set select_hashkey_col = hashkey %}
-    {% else %}
-        {% set join_hashkey_col = hashkey %}
-        {% set select_hashkey_col = new_hashkey_name %}
-    {% endif %}
-
-    {% set update_sql %}
-    UPDATE {{ satellite_relation }} sat
-    SET 
-        {{ new_hashkey_name}} = nh.{{ new_hashkey_name }},
-        {{ new_hashdiff_name}} = nh.{{ new_hashdiff_name }}
-    FROM (
-
-        SELECT 
-            sat.{{ hashkey }},
-            sat.{{ ldts_col }},
-            
-            {% if new_hashkey_name not in hash_config_dict.keys() %}
-                {# If Business Keys are not defined for parent entity, use new hashkey already existing in parent entitiy. #}
-                parent.{{ select_hashkey_col }} as {{ new_hashkey_name }},
-            {% endif %}
-
-            {{ datavault4dbt.hash_columns(columns=hash_config_dict) }}
-        FROM {{ satellite_relation }} sat
-        LEFT JOIN {{ parent_relation }} parent
-            ON sat.{{ hashkey }} = parent.{{ join_hashkey_col }}
-            
-    ) nh
-    WHERE nh.{{ ldts_col }} = sat.{{ ldts_col }}
-    AND nh.{{ hashkey }} = sat.{{ hashkey }}
-    {% endset %}
-
-    {{ return(update_sql) }}
 
 {% endmacro %}
