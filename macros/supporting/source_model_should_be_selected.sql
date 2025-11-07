5dd8688cce348fcd6a68e24331981aba5a6b8a52
mszerencse <mszerencse@scalefree.com>
2025-11-07 19:21:55 +0100

    disabling log messages

--- macros/supporting/source_model_should_be_selected.sql
+++ macros/supporting/source_model_should_be_selected.sql
@@ -1,11 +1,11 @@
 {% macro source_model_should_be_selected(source_model_name) -%}
 
-    {{ log('contect_project_name: ' ~ context['project_name'], true) }}
+    {{ log('contect_project_name: ' ~ context['project_name'], false) }}
 
     {% set model_id = 'model.' + context['project_name'] + '.' + source_model_name %}
 
-    {{ log('model_id: ' ~ model_id, true) }}
-    {{ log('selected_resources: ' ~ selected_resources, true) }}
+    {{ log('model_id: ' ~ model_id, false) }}
+    {{ log('selected_resources: ' ~ selected_resources, false) }}
     {% set model_is_selected = (model_id in selected_resources) %}
 
     {{ return(model_is_selected) }}
