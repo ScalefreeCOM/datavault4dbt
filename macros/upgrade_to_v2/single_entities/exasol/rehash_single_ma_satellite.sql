9bb2b26b4ecd9f2112e4cd92e5d8359e63c090fd
mszerencse <mszerencse@scalefree.com>
2025-12-11 08:19:11 +0100

    ma sat rehash v2

--- macros/upgrade_to_v2/single_entities/exasol/rehash_single_ma_satellite.sql
+++ macros/upgrade_to_v2/single_entities/exasol/rehash_single_ma_satellite.sql
@@ -69,7 +69,7 @@
                                                                    parent_relation=parent_relation) %}
 
     {# Executing the MERGE statement. #}
-    {{ log('Executing MERGE statement...', output_logs) }}
+    {{ log('Executing MERGE statement...'~update_sql, true) }}
     {{ '/* MERGE STATEMENT FOR ' ~ ma_satellite ~ '\n' ~ update_sql ~ '*/' }}
     {% do run_query(update_sql) %}
     {{ log('MERGE statement completed!', output_logs) }}
@@ -83,15 +83,17 @@
     {% if overwrite_hash_values %}
         {{ log('Replacing existing hash values with new ones...', output_logs) }}
 
-        {% set overwrite_sql %}
-        {{ datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=hashkey, new_col_name=hashkey + '_deprecated') }}
-        {{ datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=hashdiff, new_col_name=hashdiff + '_deprecated') }}
-        {{ datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=new_hashkey_name, new_col_name=hashkey) }}
-        {{ datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=new_hashdiff_name, new_col_name=hashdiff ) }}
-        {% endset %}
+        {% set overwrite_hkey1 = datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=hashkey, new_col_name=hashkey + '_deprecated') %}
+        {% do run_query(overwrite_hkey1) %}
 
-        {% do run_query(overwrite_sql) %}
-        
+        {% set overwrite_hkey2 = datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=new_hashkey_name, new_col_name=hashkey) %}
+        {% do run_query(overwrite_hkey2) %}
+
+        {% set overwrite_hdiff1 = datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=hashdiff, new_col_name=hashdiff + '_deprecated') %}
+        {% do run_query(overwrite_hdiff1) %}
+
+        {% set overwrite_hdiff2 = datavault4dbt.get_rename_column_sql(relation=ma_satellite_relation, old_col_name=new_hashdiff_name, new_col_name=hashdiff ) %}
+        {% do run_query(overwrite_hdiff2) %}
         
         {% if drop_old_values %}
             {{ datavault4dbt.alter_relation_add_remove_columns(relation=ma_satellite_relation, remove_columns=columns_to_drop) }}
@@ -106,7 +108,7 @@
 
 {% macro exasol__ma_satellite_update_statement(ma_satellite_relation, new_hashkey_name, new_hashdiff_name, hashkey, business_key_list, ma_keys, ldts_col, hash_config_dict, parent_relation) %}
 
-    {% set ns = namespace(update_on_condition='', parent_already_rehashed=false) %}
+    {% set ns = namespace(update_on_condition='', update_sql='', parent_already_rehashed=false) %}
 
     {% set prefixed_hashkey = 'sat.' ~ hashkey %}
 
@@ -115,7 +117,7 @@
     {% set unknown_value_rsrc = var('datavault4dbt.default_unknown_rsrc', 'SYSTEM') %}
     {% set error_value_rsrc = var('datavault4dbt.default_error_rsrc', 'ERROR') %}
 
-    {# Logic to check if parent entity is rehashed (uses the '_deprecated' column). #}
+    {# Check if parent is rehashed logic #}
     {% set all_parent_columns = adapter.get_columns_in_relation(parent_relation) %}
     {% for column in all_parent_columns %}
         {% if column.name|lower == hashkey|lower + '_deprecated' %}
@@ -130,23 +132,43 @@
         {% set join_hashkey_col = hashkey %}
         {% set select_hashkey_col = new_hashkey_name %}
     {% endif %}
-    
-    {# Build the MERGE statement #}
-    {% set update_sql %}
+
+    {%- set tmp_ns = namespace(main_hashkey_dict={}, hashdiff_names=[], hashdiff_dict={}) -%}
+
+    {%- for column in hash_config_dict.keys() -%}
+        {%- if not hash_config_dict[column].is_hashdiff -%}
+            {%- do tmp_ns.main_hashkey_dict.update({column: hash_config_dict[column]}) -%}
+        {%- elif hash_config_dict[column].is_hashdiff -%}
+            {%- do tmp_ns.hashdiff_names.append(column) -%}
+            {%- do tmp_ns.hashdiff_dict.update({column: hash_config_dict[column]}) -%}
+        {%- endif -%}
+    {%- endfor -%}
+
+    {% set ns.update_sql %}
     MERGE INTO {{ ma_satellite_relation }} sat
     USING (
 
         SELECT 
             sat.{{ hashkey }},
             sat.{{ ldts_col }},
-            {{ datavault4dbt.print_list(ma_keys) }},
+            {# Include Multi-Active Keys in the SELECT for the MERGE ON clause #}
+            {# {{ datavault4dbt.print_list(ma_keys) }},  #}
 
             {% if new_hashkey_name not in hash_config_dict.keys() %}
-                {# If Business Keys are not defined for parent entity, use new hashkey already existing in parent entity. #}
-                parent.{{ select_hashkey_col }} as {{ new_hashkey_name }},
-            {% endif %}
-            {{ datavault4dbt.hash_columns(columns=hash_config_dict, main_hashkey_column=prefixed_hashkey, multi_active_key=ma_keys) }}
+                {# If Business Keys are not defined for parent entity, use new hashkey already existing in parent entitiy. #}
+                parent.{{ select_hashkey_col }} as {{ new_hashkey_name }}
+                ,{{ datavault4dbt.hash_columns(columns=hash_config_dict, main_hashkey_column=prefixed_hashkey, multi_active_key=ma_keys) }}
 
+            {% else %}
+                {%- set processed_hash_columns = datavault4dbt.process_hash_column_excludes(tmp_ns.main_hashkey_dict) -%}
+                {{ datavault4dbt.hash_columns(columns=processed_hash_columns) }}
+                
+                {% set processed_hashdiff_columns = datavault4dbt.process_hash_column_excludes(tmp_ns.hashdiff_dict) -%}
+                {{ datavault4dbt.hash_columns(columns=processed_hashdiff_columns, multi_active_key=ma_keys, main_hashkey_column=prefixed_hashkey) }},
+      
+            {% endif %}
+            
+            
         FROM {{ ma_satellite_relation }} sat
         LEFT JOIN (
             SELECT 
@@ -157,19 +179,33 @@
         ) parent
             ON sat.{{ hashkey }} = parent.{{ join_hashkey_col }}
         WHERE sat.{{ rsrc_alias }} NOT IN ('{{ unknown_value_rsrc }}', '{{ error_value_rsrc }}')
+        
+        {#
+           The GROUP BY in the original macro is unusual for this specific SELECT
+           but is retained for fidelity. Exasol supports the syntax.
+        #}
+        GROUP BY sat.{{ hashkey }},
+                 sat.{{ ldts_col }},
+                 {# {{ datavault4dbt.print_list(ma_keys) }}, #}
+                 {{ datavault4dbt.print_list(business_key_list, src_alias='parent') }}
+                {% if new_hashkey_name not in hash_config_dict.keys() %}
+                 , parent.{{ select_hashkey_col }}                 
+                {% endif %}          
 
         UNION ALL
             
         SELECT
             sat.{{ hashkey }},
             sat.{{ ldts_col }},
+            {# Include Multi-Active Keys in the SELECT for the MERGE ON clause #}
             {{ datavault4dbt.print_list(ma_keys) }},
             sat.{{ hashkey }} AS {{ new_hashkey_name }},
             sat.{{ new_hashdiff_name | replace('_new', '') }} AS {{ new_hashdiff_name }}
         FROM {{ ma_satellite_relation }} sat
-        WHERE sat.{{ rsrc_alias }} IN ('{{ unknown_value_rsrc }}', '{{ error_value_rsrc }}')
-
+        WHERE sat.{{ rsrc_alias }} IN ('{{ unknown_value_rsrc }}', '{{ error_value_rsrc }}')          
+                    
     ) nh
+    {# Build the ON clause using Hashkey, LDTS, and MA Keys #}
     ON sat.{{ ldts_col }} = nh.{{ ldts_col }}
     AND sat.{{ hashkey }} = nh.{{ hashkey }}
     {% endset %}
@@ -179,12 +215,10 @@
         {% set where_condition %}
         AND sat.{{ datavault4dbt.escape_column_names(ma_key) }} = nh.{{ datavault4dbt.escape_column_names(ma_key) }}
         {% endset %}
-        {% set ns.update_on_condition = ns.update_on_condition + where_condition %}
+        {% set ns.update_sql = ns.update_sql + where_condition %}
     {% endfor %}
 
-    {# Complete the MERGE statement with the ON condition and UPDATE SET clause #}
-    {% set update_sql = update_sql + ns.update_on_condition %}
-    
+    {# Add the final update statement #}
     {% set update_sql_set_clause %}
     WHEN MATCHED THEN
         UPDATE SET 
@@ -192,8 +226,8 @@
             sat.{{ new_hashdiff_name}} = nh.{{ new_hashdiff_name }};
     {% endset %}
 
-    {% set update_sql = update_sql + update_sql_set_clause %}
+    {% set ns.update_sql = ns.update_sql + update_sql_set_clause %}
 
-    {{ return(update_sql) }}
+    {{ return(ns.update_sql) }}
 
 {% endmacro %}
