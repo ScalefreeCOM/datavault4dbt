592880907d44b3be55fb05b4363db5f90de05750
Deniz Polat <dpolat@scalefree.com>
2026-01-29 11:02:23 +0100

    Refactor redshift__pit macro for consistency

--- macros/tables/redshift/pit.sql
+++ macros/tables/redshift/pit.sql
@@ -1,20 +1,21 @@
-{%- macro redshift__pit(tracked_entity, hashkey, sat_names, ldts, ledts, sdts, snapshot_relation, refer_to_ghost_records, dimension_key=none, snapshot_trigger_column=none, custom_rsrc=none, pit_type=none, snapshot_optimization=false) -%}
+{%- macro redshift__pit(tracked_entity, hashkey, sat_names, ldts, ledts, sdts, snapshot_relation, dimension_key=none, refer_to_ghost_records, snapshot_trigger_column=none, custom_rsrc=none, pit_type=none, snapshot_optimization=false) -%}
 
 {%- set hash = var('datavault4dbt.hash', 'MD5') -%}
 {%- set hash_dtype = var('datavault4dbt.hash_datatype', 'VARCHAR(32)') -%}
-{%- set hash_default_values = fromjson(datavault4dbt.hash_default_values(hash_function=hash, hash_datatype=hash_dtype)) -%}
+{%- set hash_default_values = fromjson(datavault4dbt.hash_default_values(hash_function=hash,hash_datatype=hash_dtype)) -%}
 {%- set hash_alg = hash_default_values['hash_alg'] -%}
 {%- set unknown_key = hash_default_values['unknown_key'] -%}
 {%- set error_key = hash_default_values['error_key'] -%}
 {%- set string_default_dtype = datavault4dbt.string_default_dtype() -%}
 
 {%- if hash_dtype == 'BYTES' -%}
-    {%- set hashkey_string = 'TO_HEX({})'.format(datavault4dbt.prefix([hashkey], 'te')) -%}
+    {%- set hashkey_string = 'TO_HEX({})'.format(datavault4dbt.prefix([hashkey],'te')) -%}
 {%- else -%}
-    {%- set hashkey_string = datavault4dbt.prefix([hashkey], 'te') -%}
+    {%- set hashkey_string = datavault4dbt.prefix([hashkey],'te') -%}
 {%- endif -%}
 
 {%- set rsrc = var('datavault4dbt.rsrc_alias', 'rsrc') -%}
+
 {%- set beginning_of_all_times = datavault4dbt.beginning_of_all_times() -%}
 {%- set end_of_all_times = datavault4dbt.end_of_all_times() -%}
 {%- set timestamp_format = datavault4dbt.timestamp_format() -%}
@@ -31,7 +32,7 @@ SELECT
     te.{{ hashkey }},
     snap.{{ sdts }},
     {% for satellite in sat_names %}
-        {% if refer_to_ghost_records %}
+        {%- if refer_to_ghost_records -%}
             COALESCE(
                 MAX({{ satellite }}.{{ hashkey }}),
                 CAST({{ datavault4dbt.as_constant(column_str=unknown_key) }} as {{ hash_dtype }})
@@ -40,32 +41,33 @@ SELECT
                 MAX({{ satellite }}.{{ ldts }}),
                 {{ datavault4dbt.string_to_timestamp(timestamp_format, beginning_of_all_times) }}
             ) AS {{ ldts }}_{{ satellite }}
-        {% else %}
+        {%- else -%}
             MAX({{ satellite }}.{{ hashkey }}) AS hk_{{ satellite }},
             MAX({{ satellite }}.{{ ldts }}) AS {{ ldts }}_{{ satellite }}
-        {% endif %}
+        {%- endif -%}
         {{- "," if not loop.last }}
-    {%- endfor %}
+    {% endfor %}
 
 FROM {{ ref(tracked_entity) }} te
+
 {%- if datavault4dbt.is_something(snapshot_trigger_column) %}
-INNER JOIN {{ ref(snapshot_relation) }} snap
-    ON snap.{{ snapshot_trigger_column }} = true
+    INNER JOIN {{ ref(snapshot_relation) }} snap
+        ON snap.{{ snapshot_trigger_column }} = true
 {%- else %}
-CROSS JOIN {{ ref(snapshot_relation) }} snap
+    CROSS JOIN {{ ref(snapshot_relation) }} snap
 {%- endif %}
 
 {% for satellite in sat_names %}
-LEFT JOIN {{ ref(satellite) }} AS {{ satellite }}
-    ON {{ satellite }}.{{ hashkey }} = te.{{ hashkey }}
-    AND {{ satellite }}.{{ ldts }} <= snap.{{ sdts }}
+    LEFT JOIN {{ ref(satellite) }} AS {{ satellite }}
+        ON {{ satellite }}.{{ hashkey }} = te.{{ hashkey }}
+        AND {{ satellite }}.{{ ldts }} <= snap.{{ sdts }}
 {% endfor %}
 
 {%- if is_incremental() %}
-WHERE snap.{{ sdts }} NOT IN (
-    SELECT DISTINCT {{ sdts }}
-    FROM {{ this }}
-)
+    WHERE snap.{{ sdts }} NOT IN (
+        SELECT DISTINCT {{ sdts }}
+        FROM {{ this }}
+    )
 {%- endif %}
 
 GROUP BY
