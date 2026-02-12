645c779e22bbdf907ea22fd45e13ca6d01011b18
mszerencse <mszerencse@scalefree.com>
2026-02-12 12:03:35 +0100

    remove all overwrites, leave only snowflake

--- macros/rehashing/internal_overwrites/get_rename_column_sql.sql
+++ macros/rehashing/internal_overwrites/get_rename_column_sql.sql
@@ -18,16 +18,6 @@
 
 {% endmacro %}
 
-{% macro databricks__get_rename_column_sql(relation, old_col_name, new_col_name) %}
-
-    {% set query %}
-    ALTER TABLE {{ relation.render() }} RENAME COLUMN {{ old_col_name }} TO {{ new_col_name }};
-    {% endset %}
-
-    {{ return(query) }}
-
-{% endmacro %}
-
 {% macro snowflake__get_rename_column_sql(relation, old_col_name, new_col_name) %}
 
     {% if relation.is_dynamic_table -%}
@@ -39,58 +29,6 @@
     {% set query %}
     alter {{ relation.get_ddl_prefix_for_alter() }} {{ relation_type }} {{ relation.render() }} rename column {{ old_col_name }} to {{ new_col_name }};
     {% endset %}
-
-    {{ return(query) }}
-
-{% endmacro %}
-
-{% macro synapse__get_rename_column_sql(relation, old_col_name, new_col_name) %}
-
-    {% set query %}
-    EXECUTE sp_rename '{{ relation.render() }}.{{ old_col_name }}' , '{{ new_col_name }}' , 'COLUMN';
-    {% endset %}
-
-    {{ return(query) }}
-
-{% endmacro %}
-
-{% macro fabric__get_rename_column_sql(relation, old_col_name, new_col_name) %}
-
-    {% set query %}
-    EXECUTE sp_rename '{{ relation.render() }}.{{ old_col_name }}' , '{{ new_col_name }}' , 'COLUMN';
-    {% endset %}
-
-    {{ return(query) }}
-
-{% endmacro %}
-
-{% macro redshift__get_rename_column_sql(relation, old_col_name, new_col_name) %}
-
-    {% set query %}
-    alter table {{ relation.render() }} rename column {{ old_col_name }} to {{ new_col_name }};
-    {% endset %}
-
-    {{ return(query) }}
-
-{% endmacro %}
-
-{% macro postgres__get_rename_column_sql(relation, old_col_name, new_col_name) %}
-
-    {% set query %}
-    alter table {{ relation.render() }} rename column {{ old_col_name }} to {{ new_col_name }};
-    {% endset %}
-
-    {{ return(query) }}
-
-{% endmacro %}
-
-{% macro exasol__get_rename_column_sql(relation, old_col_name, new_col_name) %}
-
-    {% set query %}
-        ALTER TABLE {{ relation.render() }} RENAME COLUMN {{ old_col_name }} TO {{ new_col_name }};
-    {% endset %}
-
-    {{ log(query, false) }}
 
     {{ return(query) }}
 
