590736908d9b5039dda3ffa5c9f98817a956a00f
Tobias-Pah <tpahlke@scalefree.com>
2026-02-25 10:11:55 +0100

    fix: fixed macro call

--- macros/supporting/first_day_of_week.sql
+++ macros/supporting/first_day_of_week.sql
@@ -200,27 +200,6 @@
 
 {%- endmacro -%}
 
-{%- macro sqlserver__first_day_of_week() %}
-
-    {%- set global_var = var('datavault4dbt.first_day_of_week', none) -%}
-    {%- set first_day = 1 -%}
-
-    {%- if global_var is mapping -%}
-        {%- if 'sqlserver' in global_var.keys()|map('lower') -%}
-            {%- set first_day = global_var['sqlserver'] -%}
-        {%- else -%}
-            {%- if execute -%}
-                {%- do exceptions.warn("Warning: Adapter not found in 'datavault4dbt.first_day_of_week' dictionary. Applying default (1).") -%}
-            {%- endif -%}
-        {%- endif -%}
-
-    {%- elif global_var is not mapping and datavault4dbt.is_something(global_var) -%}
-        {%- set first_day = global_var -%}
-    {%- endif -%}
-
-    {{ return(first_day | int) }}
-
-{%- endmacro -%}
 
 {%- macro databricks__first_day_of_week() %}
 
