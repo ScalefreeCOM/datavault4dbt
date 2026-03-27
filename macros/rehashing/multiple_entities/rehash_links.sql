16f5b0f02a80b45915da72b5ac138d9b145c15e5
Theo Kiehn <162969167+tkiehn@users.noreply.github.com>
2026-03-27 08:44:40 +0100

    Minor rehashing fixes (#443)
    * various fixes
    
    * fix bigquery alter_table_add_col

--- macros/rehashing/multiple_entities/rehash_links.sql
+++ macros/rehashing/multiple_entities/rehash_links.sql
@@ -44,26 +44,29 @@
 
     {% set link_dict = fromyaml(link_yaml) %}
 
-    {% set overwrite_hash_values = link_dict.config.get('overwrite_hash_values', false) %}
+    {% if link_dict.get('links') is not none %}
 
-    {% for link in link_dict.get('links') %}
-        {% set specific_link_overwrite_hash = link.get('overwrite_hash_values', overwrite_hash_values) %}
-         
-        {% set columns_to_drop_list =  datavault4dbt.rehash_single_link(link=link.name, 
-                                                                        link_hashkey=link.link_hashkey,
-                                                                        additional_hash_input_cols=link.additional_hash_input_cols,
-                                                                        overwrite_hash_values=specific_link_overwrite_hash,
-                                                                        hub_config=link.hub_config,
-                                                                        output_logs=false,
-                                                                        drop_old_values=drop_old_values) %}
-                        
-        {{ log(link.name ~ ' rehashed successfully.', true) }}      
+        {% set overwrite_hash_values = link_dict.config.get('overwrite_hash_values', false) %}
 
-        {% set columns_to_drop_dict = {'model_name': link.name, 'columns_to_drop': (columns_to_drop_list | trim)} %}
+        {% for link in link_dict.get('links') %}
+            {% set specific_link_overwrite_hash = link.get('overwrite_hash_values', overwrite_hash_values) %}
+            {% set additional_hash_input_cols = link.additional_hash_input_cols if link.additional_hash_input_cols is not undefined else [] %}
+            {% set columns_to_drop_list =  datavault4dbt.rehash_single_link(link=link.name, 
+                                                                            link_hashkey=link.link_hashkey,
+                                                                            additional_hash_input_cols=additional_hash_input_cols,
+                                                                            overwrite_hash_values=specific_link_overwrite_hash,
+                                                                            hub_config=link.hub_config,
+                                                                            output_logs=false,
+                                                                            drop_old_values=drop_old_values) %}
+                            
+            {{ log(link.name ~ ' rehashed successfully.', true) }}
 
-        {% do ns.columns_to_drop.append(columns_to_drop_dict) %}
+            {% set columns_to_drop_dict = {'model_name': link.name, 'columns_to_drop': (columns_to_drop_list | trim)} %}
 
-    {% endfor %}
+            {% do ns.columns_to_drop.append(columns_to_drop_dict) %}
+
+        {% endfor %}
+    {% endif %}
 
     {{ return(ns.columns_to_drop) }}
 
