414e50c9d0d39a895379f103c9b316304cdc34cf
jstendel <jstendel@scalefree.com>
2026-05-19 15:23:48 +0200

    feat(pit): automatically exclude ghost hub rows when mandatory filtering is active

--- macros/tables/redshift/pit.sql
+++ macros/tables/redshift/pit.sql
@@ -119,6 +119,7 @@ GROUP BY
 {%- if mandatory_conditions | length > 0 %}
 HAVING
     {{ '(' ~ mandatory_conditions | join(' OR ') ~ ')' if (datavault4dbt.is_something(mandatory_strategy) and mandatory_strategy | lower == 'any') else mandatory_conditions | join(' AND ') }}
+    AND te.{{ hashkey }} != {{ datavault4dbt.as_constant(column_str=unknown_key) }}
 {%- endif %}
 
 {%- endmacro -%}
